633dc1390b603008c13f12133b478d0186fb1213
Craig Miller <craigmiller160@gmail.com>
2022-01-11 17:39:01 -0500

    Setup initial hello world in UI

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <h1>Hello World</h1>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
