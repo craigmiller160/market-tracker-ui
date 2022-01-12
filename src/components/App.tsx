35d0bb65dc17bfbe4f22acba1149d0bf8736fcf7
Craig Miller <craigmiller160@gmail.com>
2022-01-12 12:05:46 -0500

    Playing with components and styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,17 @@
 import { Button } from 'antd';
+import {Navbar} from './Navbar';
 
 export const App = () => {
 	const onClick = () => alert('Clicked');
 
 	return (
 		<div>
-			<Button type="primary" onClick={onClick}>
-				Click Me
-			</Button>
+            <Navbar />
+            <div style={ { margin: '5rem auto', width: '100%', textAlign: 'center' } }>
+                <Button type="primary" onClick={onClick}>
+                    Click Me
+                </Button>
+            </div>
 		</div>
 	);
 };
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import {Menu} from 'antd';
+import './Navbar.scss';
+
+export const Navbar = () => {
+    return (
+        <Menu className="Navbar">
+            <Menu.Item key="first">First</Menu.Item>
+        </Menu>
+    )
+};
