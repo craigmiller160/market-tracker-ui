82985046fbcfe14619f3f50872cbf5a2604aff98
Craig <craigmiller160@gmail.com>
2022-01-11 21:13:25 -0500

    Got linting working

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,8 @@ module.exports = {
         '@craigmiller160/eslint-config-jest',
         '@craigmiller160/eslint-config-tree-shaking-import-restrictions',
         '@craigmiller160/eslint-config-ts'
-    ]
+    ],
+    rules: {
+        'react/react-in-jsx-scope': 0
+    }
 }
