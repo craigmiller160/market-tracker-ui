0a3a1745af087b4f699587c259255081b8451083
Craig Miller <craigmiller160@gmail.com>
2022-01-12 12:08:23 -0500

    Making progress with navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,14 @@
 import {Menu} from 'antd';
 import './Navbar.scss';
+import { MenuInfo } from 'rc-menu/lib/interface';
 
 export const Navbar = () => {
+    const handleClick = (e: MenuInfo) => {
+        console.log('Event', e);
+    };
+
     return (
-        <Menu className="Navbar">
+        <Menu className="Navbar" onClick={handleClick}>
             <Menu.Item key="first">First</Menu.Item>
         </Menu>
     )
