81eb6e007e72d99416b7d15bdb13f8294ca6d2fe
Craig Miller <craigmiller160@gmail.com>
2022-01-12 11:35:50 -0500

    Working on trying out antd

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,19 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import 'antd/dist/antd.css';
 import ReactDOM from 'react-dom';
+import { Button } from 'antd';
 
-const App = () => <h1>Hello World</h1>;
+const App = () => {
+	const onClick = () => alert('Clicked');
+
+	return (
+		<div>
+			<Button type="primary" onClick={onClick}>
+				Click Me
+			</Button>
+		</div>
+	);
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
