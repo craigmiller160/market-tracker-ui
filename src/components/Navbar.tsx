fa7d48ef3f1c48742d6d0235e00b0288b81626b2
Craig Miller <craigmiller160@gmail.com>
2022-01-12 13:20:45 -0500

    Cleaning stuff up

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,50 @@
-import {Menu} from 'antd';
+import {Button, Menu} from 'antd';
 import './Navbar.scss';
 import { MenuInfo } from 'rc-menu/lib/interface';
-import { MailOutlined } from '@ant-design/icons'
+import {MailOutlined, MenuFoldOutlined, MenuUnfoldOutlined} from '@ant-design/icons'
 import {useState} from 'react';
 
 export const Navbar = () => {
     const [state, setState] = useState({
-        selected: 'first'
+        selected: 'first',
+        showSidebar: false
     });
     const handleClick = (e: MenuInfo) => {
-        console.log('Event', e);
-        setState({
-            selected: e.key
-        });
+        if (e.key === 'sidebar') {
+            setState((prevState) => ({
+                ...prevState,
+                showSidebar: !prevState.showSidebar
+            }));
+        } else {
+            setState({
+                selected: e.key,
+                showSidebar: false
+            });
+        }
     };
 
+    const SidebarButtonIcon = state.showSidebar ? MenuUnfoldOutlined : MenuFoldOutlined;
+    console.log('Sidebar', state.showSidebar)
+
     return (
-        <Menu className="Navbar" theme="dark" onClick={handleClick} mode="horizontal" selectedKeys={[ state.selected ]}>
-            <Menu.Item key="first" icon={<MailOutlined />}>First</Menu.Item>
-            <Menu.Item key="second" icon={<MailOutlined />}>Second</Menu.Item>
-            <Menu.Item style={ { marginLeft: 'auto' } } key="third" icon={<MailOutlined />}>Third</Menu.Item>
-            <Menu.Item key="fourth" icon={<MailOutlined />}>Fourth</Menu.Item>
-        </Menu>
+        <>
+            <Menu className="Navbar" theme="dark" onClick={handleClick} mode="horizontal" selectedKeys={[ state.selected ]}>
+                <Menu.Item key="sidebar">
+                    <Button type="primary">
+                        <SidebarButtonIcon />
+                    </Button>
+                </Menu.Item>
+                <Menu.Item key="first" icon={<MailOutlined />}>First</Menu.Item>
+                <Menu.Item key="second" icon={<MailOutlined />}>Second</Menu.Item>
+                <Menu.Item style={ { marginLeft: 'auto' } } key="third" icon={<MailOutlined />}>Third</Menu.Item>
+                <Menu.Item key="fourth" icon={<MailOutlined />}>Fourth</Menu.Item>
+            </Menu>
+            <Menu theme="dark" mode="inline" inlineCollapsed={ !state.showSidebar } selectedKeys={[ state.selected ]}>
+                <Menu.Item key="first" icon={<MailOutlined />}>First</Menu.Item>
+                <Menu.Item key="second" icon={<MailOutlined />}>Second</Menu.Item>
+                <Menu.Item key="third" icon={<MailOutlined />}>Third</Menu.Item>
+                <Menu.Item key="fourth" icon={<MailOutlined />}>Fourth</Menu.Item>
+            </Menu>
+        </>
     )
 };
