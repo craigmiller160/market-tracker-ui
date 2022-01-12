badcc221a061bc783fa3e4773eb2db397ad8d452
Craig <craigmiller160@gmail.com>
2022-01-11 21:14:38 -0500

    Added jest support

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+const merge = require('@craigmiller160/config-merge');
+const jestConfig = require('@craigmiller160/jest-config');
+const jestTsConfig = require('@craigmiller160/jest-config-ts');
+
+module.exports = merge(jestConfig, jestTsConfig);
