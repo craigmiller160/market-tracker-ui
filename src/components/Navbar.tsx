a9cc6d8d4b115ae1a84729bcaef081a5408cbc08
Craig Miller <craigmiller160@gmail.com>
2022-01-12 12:11:47 -0500

    Lots of progress with navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,24 @@
 import {Menu} from 'antd';
 import './Navbar.scss';
 import { MenuInfo } from 'rc-menu/lib/interface';
+import { MailOutlined } from '@ant-design/icons'
+import {useState} from 'react';
 
 export const Navbar = () => {
+    const [state, setState] = useState({
+        selected: 'first'
+    });
     const handleClick = (e: MenuInfo) => {
         console.log('Event', e);
+        setState({
+            selected: e.key
+        });
     };
 
     return (
-        <Menu className="Navbar" onClick={handleClick}>
-            <Menu.Item key="first">First</Menu.Item>
+        <Menu className="Navbar" onClick={handleClick} mode="horizontal" selectedKeys={[ state.selected ]}>
+            <Menu.Item key="first" icon={<MailOutlined />}>First</Menu.Item>
+            <Menu.Item key="second" icon={<MailOutlined />}>Second</Menu.Item>
         </Menu>
     )
 };
