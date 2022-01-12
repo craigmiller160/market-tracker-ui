4916f5bc31e3cdbe74d7a64633c164a20c5e8467
Craig Miller <craigmiller160@gmail.com>
2022-01-12 16:53:45 -0500

    Integrated auth check into Content page

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,8 +1,18 @@
 import { Welcome } from './Welcome';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadAuthUser } from '../../store/auth/actions';
+import { RootState } from '../../store';
 
 // TODO content disappears on smaller screen... why?
-export const Content = () => (
-	<div>
-		<Welcome />
-	</div>
-);
+export const Content = () => {
+	const dispatch = useDispatch();
+	const hasChecked = useSelector<RootState, boolean>(
+		(_) => _.auth.hasChecked
+	);
+	useEffect(() => {
+		dispatch(loadAuthUser());
+	}, [dispatch]);
+
+	return <div>{hasChecked && <Welcome />}</div>;
+};
