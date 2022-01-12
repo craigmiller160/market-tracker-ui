786290706bc3170775807ac0dfef657e6b5f8bb4
Craig Miller <craigmiller160@gmail.com>
2022-01-12 15:43:40 -0500

    Moved code into oldReference directory

---
+++ src/components/oldReference/App.tsx
@@ -0,0 +1,19 @@
+import { Button } from 'antd';
+import {Navbar} from './Navbar';
+import {Chart} from './Chart';
+
+export const App = () => {
+	const onClick = () => alert('Clicked');
+
+	return (
+		<div>
+            <Navbar />
+            <div style={ { margin: '5rem auto', width: '100%', textAlign: 'center' } }>
+                <Button type="primary" onClick={onClick}>
+                    Click Me
+                </Button>
+                <Chart />
+            </div>
+		</div>
+	);
+};
