6a06fe9ed45548b89e60b2e333af658f02134b58
Craig Miller <craigmiller160@gmail.com>
2022-01-12 13:30:41 -0500

    Stubbed out chart component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import { Button } from 'antd';
 import {Navbar} from './Navbar';
+import {Chart} from './Chart';
 
 export const App = () => {
 	const onClick = () => alert('Clicked');
@@ -11,6 +12,7 @@ export const App = () => {
                 <Button type="primary" onClick={onClick}>
                     Click Me
                 </Button>
+                <Chart />
             </div>
 		</div>
 	);
---
+++ src/components/Chart.tsx
@@ -0,0 +1,8 @@
+
+export const Chart = () => {
+    return (
+        <div>
+            <h1>Chart Goes Here</h1>
+        </div>
+    )
+};
