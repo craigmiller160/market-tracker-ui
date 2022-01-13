544d55c827a17d15c9921fb64ae05d4281d671f2
Craig Miller <craigmiller160@gmail.com>
2022-01-13 15:54:55 -0500

    Improved navbar to include brand/title

--- src/components/Navbar/DesktopNavbar.tsx
+++ src/components/Navbar/DesktopNavbar.tsx
@@ -1,4 +1,4 @@
-import { Menu } from 'antd';
+import { Menu, Layout } from 'antd';
 import './DesktopNavbar.scss';
 import { FC } from 'react';
 import { NavbarProps } from './NavbarProps';
@@ -6,18 +6,22 @@ import { NavbarProps } from './NavbarProps';
 export const DesktopNavbar: FC<NavbarProps> = (props) => {
 	const { selected, handleMenuClick } = props;
 	return (
-		<Menu
-			onClick={handleMenuClick}
-			className="DesktopNavbar"
-			theme="dark"
-			mode="horizontal"
-			selectedKeys={[selected]}
-		>
-			<Menu.Item key="portfolios">Portfolios</Menu.Item>
-			<Menu.Item key="watchlists">Watchlists</Menu.Item>
-			<Menu.Item className="AuthItem" key="auth">
-				Login
-			</Menu.Item>
-		</Menu>
+		<Layout.Header className="DesktopNavbar">
+			<div className="Brand">
+				<h3 style={{ color: 'white' }}>Market Tracker</h3>
+			</div>
+			<Menu
+				onClick={handleMenuClick}
+				theme="dark"
+				mode="horizontal"
+				selectedKeys={[selected]}
+			>
+				<Menu.Item key="portfolios">Portfolios</Menu.Item>
+				<Menu.Item key="watchlists">Watchlists</Menu.Item>
+				<Menu.Item className="AuthItem" key="auth">
+					Login
+				</Menu.Item>
+			</Menu>
+		</Layout.Header>
 	);
 };
