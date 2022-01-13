75b344c137bb4c386d527de50e9d4365d247dc8d
Craig Miller <craigmiller160@gmail.com>
2022-01-13 16:07:49 -0500

    More redux selectors

--- src/store/auth/selectors.ts
+++ src/store/auth/selectors.ts
@@ -1,4 +1,15 @@
 import { RootState } from '../index';
+import { createSelector } from '@reduxjs/toolkit';
+import * as O from 'fp-ts/es6/Option';
+import { AuthUser } from '../../types/auth';
 
 export const hasCheckedSelector = (state: RootState): boolean =>
 	state.auth.hasChecked;
+
+const userDataSelector = (state: RootState): O.Option<AuthUser> =>
+	state.auth.userData;
+
+export const isAuthorized = createSelector(
+	userDataSelector,
+	(userData: O.Option<AuthUser>) => O.isSome(userData)
+);
