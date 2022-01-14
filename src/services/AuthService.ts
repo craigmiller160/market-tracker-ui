efefd3db4272cb041dd2e14aa871ac39df9fb2a7
Craig Miller <craigmiller160@gmail.com>
2022-01-14 10:28:04 -0500

    Implemented more service methods

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,5 +1,5 @@
 import * as TE from 'fp-ts/es6/TaskEither';
-import { AuthUser } from '../types/auth';
+import {AuthCodeLogin, AuthUser} from '../types/auth';
 import { ajaxApi } from './AjaxApi';
 import { pipe } from 'fp-ts/es6/function';
 import { isAxiosError } from '@craigmiller160/ajax-api-fp-ts';
@@ -20,4 +20,27 @@ export const getAuthUser = (): TE.TaskEither<Error, AuthUser> =>
 			}
 		}),
 		TE.map((res: AxiosResponse<AuthUser>) => res.data)
+	);
+
+export const login = (): TE.TaskEither<Error, AuthCodeLogin> =>
+	pipe(
+		ajaxApi.post<void, AuthCodeLogin>({
+			uri: '/oauth/authcode/login',
+			errorMsg: 'Error getting login URI'
+		}),
+		TE.map((_) => _.data),
+		TE.map((_) => {
+			window.location.assign(_.url);
+			return _;
+		})
+	);
+
+// TODO implement this server-side
+export const logout = (): TE.TaskEither<Error, void> =>
+	pipe(
+		ajaxApi.get<void>({
+			uri: '/oauth/logout',
+			errorMsg: 'Error logging out'
+		}),
+		TE.map((_) => _.data)
 	);
