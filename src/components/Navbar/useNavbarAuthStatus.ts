e96e8b05b4c9e541f12ac17a3bb2229151a5c20f
Craig Miller <craigmiller160@gmail.com>
2022-01-14 10:29:04 -0500

    Integrated authBtnAction into the navbar auth check

--- src/components/Navbar/useNavbarAuthStatus.ts
+++ src/components/Navbar/useNavbarAuthStatus.ts
@@ -1,10 +1,13 @@
 import { useSelector } from 'react-redux';
 import { isAuthorizedSelector } from '../../store/auth/selectors';
+import * as TE from 'fp-ts/es6/TaskEither';
+import { login, logout } from '../../services/AuthService';
 
-export type NavbarAuth = [boolean, string, () => void];
+export type NavbarAuth = [boolean, string, () => TE.TaskEither<Error, unknown>];
 
 export const useNavbarAuthCheck = (): NavbarAuth => {
 	const isAuthorized = useSelector(isAuthorizedSelector);
 	const authBtnTxt = isAuthorized ? 'Logout' : 'Login';
-	return [isAuthorized, authBtnTxt, () => {}];
+	const authBtnAction = isAuthorized ? login : logout;
+	return [isAuthorized, authBtnTxt, authBtnAction];
 };
