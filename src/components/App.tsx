d067326f0585541334cf6e0fbd62720a39799936
Craig Miller <craigmiller160@gmail.com>
2022-01-15 18:21:17 -0500

    Major refactor of how the initial routes are setup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,11 +3,12 @@ import { Content } from './Content';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from '../store';
-import { Layout } from 'antd';
 import './App.scss';
 import useBreakpoint from 'antd/es/grid/hooks/useBreakpoint';
 import { Breakpoints } from './utils/Breakpoints';
 import { ScreenContextValue, ScreenContext } from './ScreenContext';
+import { Navigate, Routes, Route } from 'react-router-dom';
+import { Layout } from 'antd';
 
 const createScreenContextValue = (
 	breakpoints: Breakpoints
@@ -15,18 +16,28 @@ const createScreenContextValue = (
 	breakpoints
 });
 
-// TODO build in redirect from / to /market-tracker
 export const App = () => {
 	const breakpoints = useBreakpoint();
 	const screenContextValue = createScreenContextValue(breakpoints);
 	return (
 		<ScreenContext.Provider value={screenContextValue}>
 			<Provider store={store}>
-				<BrowserRouter basename="/market-tracker">
-					<Layout className="MarketTrackerApp">
-						<Navbar />
-						<Content />
-					</Layout>
+				<BrowserRouter basename="/">
+					<Routes>
+						<Route
+							path="/"
+							element={<Navigate to="/market-tracker" />}
+						/>
+						<Route
+							path="/market-tracker"
+							element={
+								<Layout className="MarketTrackerApp">
+									<Navbar />
+									<Content />
+								</Layout>
+							}
+						/>
+					</Routes>
 				</BrowserRouter>
 			</Provider>
 		</ScreenContext.Provider>
