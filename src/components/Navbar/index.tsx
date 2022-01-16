6f73effa000dbab2a6a81c0cbc7a0b6a8f7d8801
Craig Miller <craigmiller160@gmail.com>
2022-01-16 11:56:06 -0500

    Integrating route navigation

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -9,6 +9,7 @@ import { MenuItemKey } from './MenuItemKey';
 import { NavbarProps } from './NavbarProps';
 import { ScreenContext } from '../ScreenContext';
 import { useNavbarAuthCheck } from './useNavbarAuthStatus';
+import { NavigateFunction, useNavigate } from 'react-router';
 
 interface State {
 	selected: MenuItemKey;
@@ -19,25 +20,27 @@ const initState: State = {
 };
 
 const createHandleMenuClick =
-	(setState: Updater<State>) =>
+	(setState: Updater<State>, navigate: NavigateFunction) =>
 	(menuItemInfo: MenuInfo): void =>
 		match(menuItemInfo.key as MenuItemKey)
 			.with('auth', () => {
 				// Do nothing for now
 			})
-			.otherwise((_) =>
+			.otherwise((key) => {
+				navigate(`/market-tracker/${key}`);
 				setState((draft) => {
-					draft.selected = _;
-				})
-			);
+					draft.selected = key;
+				});
+			});
 
 export const Navbar: FC<object> = () => {
+	const navigate = useNavigate();
 	const { breakpoints } = useContext(ScreenContext);
 	const [state, setState] = useImmer<State>(initState);
 	const [isAuthorized, hasChecked, authBtnTxt, authBtnAction] =
 		useNavbarAuthCheck();
 
-	const handleMenuClick = createHandleMenuClick(setState);
+	const handleMenuClick = createHandleMenuClick(setState, navigate);
 
 	const props: NavbarProps = {
 		selected: state.selected,
