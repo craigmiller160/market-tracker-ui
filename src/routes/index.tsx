d09de16ff7476d93be4b03ba414bf2b2116632fc
Craig <craigmiller160@gmail.com>
2022-01-16 10:28:24 -0500

    Lots of code cleanup of routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -20,45 +20,48 @@ export const routes = (rules: RouteRules): RouteObject[] => [
 		children: match(rules.hasChecked)
 			.with(true, () =>
 				match(rules.isAuthorized)
-					.with(true, () => {
-						console.log('IsChecked & IsAuthorized')
-						return [
-							{
-								path: 'portfolios',
-								element: <Portfolios />
-							},
-							{
-								path: 'watchlists',
-								element: <Watchlists />
-							},
-							{
-								path: '*',
-								element: <Navigate to="portfolios" />
-							}
-						]
-					})
-					.otherwise(() => {
-						console.log('IsCheckedOnly')
-						return [
-							{
-								path: 'welcome',
-								element: <Welcome />
-							},
-							{
-								path: '',
-								element: <Navigate to="welcome" />
-							}
-						]
-					})
+					.with(true, () => [
+						{
+							path: 'portfolios',
+							element: <Portfolios />
+						},
+						{
+							path: 'watchlists',
+							element: <Watchlists />
+						},
+						{
+							path: '',
+							element: <Navigate to="portfolios" />
+						},
+						{
+							path: '*',
+							element: <Navigate to="portfolios" />
+						}
+					])
+					.otherwise(() => [
+						{
+							path: 'welcome',
+							element: <Welcome />
+						},
+						{
+							path: '',
+							element: <Navigate to="welcome" />
+						},
+						{
+							path: '*',
+							element: <Navigate to="welcome" />
+						}
+					])
 			)
-			.otherwise(() => {
-				console.log('AllFalse')
-				return [
-					{
-						path: '*',
-						element: <div />
-					}
-				]
-			})
+			.otherwise(() => [
+				{
+					path: '',
+					element: <div />
+				},
+				{
+					path: '*',
+					element: <div />
+				}
+			])
 	}
 ];
