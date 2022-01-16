271c622f2ba8b592d88ef17c209bcc9cdd4f4601
Craig Miller <craigmiller160@gmail.com>
2022-01-16 17:46:15 -0500

    Began stubbing out tests

---
+++ test/components/Navbar/Navbar.test.ts
@@ -0,0 +1,27 @@
+export {};
+
+describe('Navbar', () => {
+	it('renders for desktop', () => {
+		throw new Error();
+	});
+
+	it('renders for mobile', () => {
+		throw new Error();
+	});
+
+	it('shows correct items for un-authenticated user', () => {
+		throw new Error();
+	});
+
+	it('shows correct items for authenticated user', () => {
+		throw new Error();
+	});
+
+	it('navigates to portfolios page', () => {
+		throw new Error();
+	});
+
+	it('navigates to watchlists page', () => {
+		throw new Error();
+	});
+});
