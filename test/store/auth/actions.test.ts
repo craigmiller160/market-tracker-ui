e1e8a097b2df4288cd884758d14fc987906d90c8
Craig Miller <craigmiller160@gmail.com>
2022-01-16 18:11:21 -0500

    Implemented more tests

--- test/store/auth/actions.test.ts
+++ test/store/auth/actions.test.ts
@@ -1,13 +1,54 @@
-export {};
+import { ajaxApi } from '../../../src/services/AjaxApi';
+import MockAdapter from 'axios-mock-adapter';
+import thunk from 'redux-thunk';
+import createMockStore from 'redux-mock-store';
+import { loadAuthUser } from '../../../src/store/auth/actions';
+import { RootState } from '../../../src/store';
+import { AnyAction, ThunkDispatch } from '@reduxjs/toolkit';
+import * as Option from 'fp-ts/es6/Option';
+import { authSlice } from '../../../src/store/auth/slice';
+import { AuthUser } from '../../../src/types/auth';
+
+type DispatchExts = ThunkDispatch<RootState, void, AnyAction>;
+
+const authUser: AuthUser = {
+	userId: 1
+};
+
+const mockApi = new MockAdapter(ajaxApi.instance);
+const mockStore = createMockStore<RootState, DispatchExts>([thunk])({
+	auth: {
+		hasChecked: false,
+		userData: Option.none
+	}
+});
 
 describe('auth actions', () => {
+	beforeEach(() => {
+		mockApi.reset();
+		mockStore.clearActions();
+	});
+
 	describe('loadAuthUser', () => {
-		it('user is authenticated', () => {
-			throw new Error();
+		it('user is authenticated', async () => {
+			mockApi.onGet('/oauth/user').reply(200, authUser);
+			await mockStore.dispatch(loadAuthUser());
+			expect(mockStore.getActions()).toEqual([
+				{
+					type: authSlice.actions.setUserData.type,
+					payload: Option.some(authUser)
+				}
+			]);
 		});
 
-		it('user is not authenticated', () => {
-			throw new Error();
+		it('user is not authenticated', async () => {
+			await mockStore.dispatch(loadAuthUser());
+			expect(mockStore.getActions()).toEqual([
+				{
+					type: authSlice.actions.setUserData.type,
+					payload: Option.none
+				}
+			]);
 		});
 	});
 });
