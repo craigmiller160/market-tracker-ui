8ae093257e566a5cf37e733c1f5f9388497368c9
Craig Miller <craigmiller160@gmail.com>
2022-01-16 17:54:57 -0500

    Stubbed out even more tests

---
+++ test/store/auth/selectors.test.ts
@@ -0,0 +1,13 @@
+export {};
+
+describe('selectors', () => {
+	describe('isAuthorizedSelector', () => {
+		it('is authorized', () => {
+			throw new Error();
+		});
+
+		it('is not authorized', () => {
+			throw new Error();
+		});
+	});
+});
