6254dcbf406675e567e4a10715462b6250aa6a6a
Craig Miller <craigmiller160@gmail.com>
2022-01-16 17:57:26 -0500

    Implemented the first test

--- test/store/auth/slice.test.ts
+++ test/store/auth/slice.test.ts
@@ -1,7 +1,20 @@
-export {};
+import { authSlice } from '../../../src/store/auth/slice';
+import * as Option from 'fp-ts/es6/Option';
+import { AuthUser } from '../../../src/types/auth';
+
+const authUser: AuthUser = {
+	userId: 1
+};
 
 describe('auth slice', () => {
 	it('setUserData', () => {
-		throw new Error();
+		const result = authSlice.reducer(
+			authSlice.getInitialState(),
+			authSlice.actions.setUserData(Option.some(authUser))
+		);
+		expect(result).toEqual({
+			hasChecked: true,
+			userData: Option.some(authUser)
+		});
 	});
 });
