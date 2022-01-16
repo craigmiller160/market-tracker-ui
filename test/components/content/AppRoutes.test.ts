8729cfee06c95dab592d64ba9a9f2f1744905874
Craig Miller <craigmiller160@gmail.com>
2022-01-16 18:58:42 -0500

    Stubbed out tests for routing

---
+++ test/components/content/AppRoutes.test.ts
@@ -0,0 +1,19 @@
+export {};
+
+describe('AppRoutes', () => {
+	it('shows correct initial route for un-authenticated user', () => {
+		throw new Error();
+	});
+
+	it('shows correct initial route for authenticated user', () => {
+		throw new Error();
+	});
+
+	it('navigates to portfolios page', () => {
+		throw new Error();
+	});
+
+	it('navigates to watchlists page', () => {
+		throw new Error();
+	});
+});
