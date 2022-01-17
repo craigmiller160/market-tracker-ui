afd9ef3d9330d9f0167792b5b428fafbecb9ee27
Craig <craigmiller160@gmail.com>
2022-01-17 08:16:02 -0500

    Test setup logic

--- test/components/content/AppRoutes.test.ts
+++ test/components/content/AppRoutes.test.ts
@@ -1,6 +1,15 @@
-export {};
+import { ajaxApi } from '../../../src/services/AjaxApi';
+import MockAdapter from 'axios-mock-adapter';
+import { createRenderApp } from '../../testutils/RenderApp';
+
+const mockApi = new MockAdapter(ajaxApi.instance);
+const renderApp = createRenderApp(mockApi);
 
 describe('AppRoutes', () => {
+	beforeEach(() => {
+		mockApi.reset();
+	});
+
 	it('shows correct initial route for un-authenticated user', () => {
 		throw new Error();
 	});
