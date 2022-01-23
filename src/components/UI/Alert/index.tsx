f2a53c522bfe702722e0f076b70901834e4c97ac
Craig <craigmiller160@gmail.com>
2022-01-23 09:22:45 -0500

    Got alert styled properly

--- src/components/UI/Alert/index.tsx
+++ src/components/UI/Alert/index.tsx
@@ -1,15 +1,18 @@
 import { Alert as AntAlert } from 'antd';
+import './Alert.scss';
 
 // TODO need tests for this
 
 export const Alert = () => {
 	return (
-		<AntAlert
-			type="success"
-			message="Hello Success"
-			description="The detailed message"
-			showIcon
-			closable
-		/>
+		<div className="AlertWrapper">
+			<AntAlert
+				type="success"
+				message="Hello Success"
+				description="The detailed message"
+				showIcon
+				closable
+			/>
+		</div>
 	);
 };
