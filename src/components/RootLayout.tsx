4a7e5f957d8bb8b303020d8dcda600712ae8ebfd
Craig Miller <craigmiller160@gmail.com>
2022-01-28 16:24:22 -0500

    Final styling tweaks for mobile layout

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,12 +1,11 @@
 import { Navbar } from './Navbar';
 import { Content } from './Content';
 import { Layout } from 'antd';
+import './RootLayout.scss';
 
 export const RootLayout = () => (
-	<>
-		<Layout>
-			<Navbar />
-			<Content />
-		</Layout>
-	</>
+	<Layout className="RootLayout">
+		<Navbar />
+		<Content />
+	</Layout>
 );
