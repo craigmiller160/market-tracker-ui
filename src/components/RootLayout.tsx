26b6c27eba5bdd5a720062fd99e42fdc159d9489
Craig Miller <craigmiller160@gmail.com>
2022-01-28 16:28:43 -0500

    Feature/mt 26 (#5)
    * Added nginx.conf and internal certs
    
    * Implemented nginx.conf
    
    * Added the Dockerfile
    
    * Added deployment file
    
    * Updated .gitignore
    
    * Fixed package.json config for files to publish
    
    * Fixed project version number
    
    * Fixing nginx.conf bugs
    
    * Added ConfigProvider to hopefully fix CSP
    
    * Deleted old reference components
    
    * Figured out how to make the layout work
    
    * Final styling tweaks for mobile layout

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,9 +1,11 @@
 import { Navbar } from './Navbar';
 import { Content } from './Content';
+import { Layout } from 'antd';
+import './RootLayout.scss';
 
 export const RootLayout = () => (
-	<>
+	<Layout className="RootLayout">
 		<Navbar />
 		<Content />
-	</>
+	</Layout>
 );
