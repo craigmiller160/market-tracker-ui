0f449d0997f88d71abdd07451e0ba90c28466260
Craig Miller <craigmiller160@gmail.com>
2022-01-30 18:33:41 -0500

    Adjusted page layout size for different screen sizes

--- src/components/Content/Markets/index.tsx
+++ src/components/Content/Markets/index.tsx
@@ -3,10 +3,21 @@ import { useMarketData } from './useMarketData';
 import './Markets.scss';
 import { MarketCard } from './MarketCard';
 import { MarketData } from './MarketData';
+import { useContext } from 'react';
+import { ScreenContext } from '../../ScreenContext';
+import { Breakpoints } from '../../utils/Breakpoints';
+import { match } from 'ts-pattern';
 
 const marketDataToCard = (data: MarketData) => (
 	<MarketCard key={data.symbol} data={data} />
 );
+
+const getPageSizeClass = (breakpoints: Breakpoints): string =>
+	match(breakpoints)
+		.with({ xxl: true }, () => 'XXL')
+		.with({ xxl: false, xl: true }, () => 'XL')
+		.with({ xxl: false, xl: false, lg: true }, () => 'LG')
+		.otherwise(() => '');
 
 // TODO add spinners
 
@@ -14,9 +25,12 @@ export const Markets = () => {
 	const { usMarketData, internationalMarketData } = useMarketData();
 	const UsMarketCards = usMarketData.map(marketDataToCard);
 	const IntMarketCards = internationalMarketData.map(marketDataToCard);
+	const { breakpoints } = useContext(ScreenContext);
+	const pageSizeClass = getPageSizeClass(breakpoints);
+	const className = `GlobalMarkets ${pageSizeClass}`;
 
 	return (
-		<div className="GlobalMarkets">
+		<div className={className}>
 			<Typography.Title>All Global Markets</Typography.Title>
 			<section className="MarketSection">
 				<Typography.Title level={3}>US Markets</Typography.Title>
