838ee01b629c129fbb6a6126b314d92eb1a17763
Craig Miller <craigmiller160@gmail.com>
2022-01-30 12:53:40 -0500

    Cleaned up temporary code

--- src/components/Content/Markets/index.tsx
+++ src/components/Content/Markets/index.tsx
@@ -1,18 +1,1 @@
-import { useEffect } from 'react';
-import { pipe } from 'fp-ts/es6/function';
-import * as tradierService from '../../../services/TradierService';
-import * as TaskEither from 'fp-ts/es6/TaskEither';
-
-export const Markets = () => {
-    // TODO delete this temporary logic
-	useEffect(() => {
-		pipe(
-			tradierService.getHistoryQuote('VTI'),
-			TaskEither.fold(
-				(ex) => async () => console.error(ex),
-				(data) => async () => console.log(JSON.stringify(data))
-			)
-		)();
-	}, []);
-	return <h1>Markets Page</h1>;
-};
+export const Markets = () => <h1>Markets Page</h1>;
