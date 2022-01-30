ac2098753841cdc34eb4553cef3cc6934fb0aa81
Craig Miller <craigmiller160@gmail.com>
2022-01-30 17:58:52 -0500

    Code cleanup

--- src/components/Content/Markets/index.tsx
+++ src/components/Content/Markets/index.tsx
@@ -2,15 +2,18 @@ import { Typography } from 'antd';
 import { useMarketData } from './useMarketData';
 import './Markets.scss';
 import { MarketCard } from './MarketCard';
+import { MarketData } from './MarketData';
+
+const marketDataToCard = (data: MarketData) => (
+	<MarketCard key={data.symbol} data={data} />
+);
+
+// TODO add spinners
 
 export const Markets = () => {
 	const { usMarketData, internationalMarketData } = useMarketData();
-	const UsMarketCards = usMarketData.map((data) => (
-		<MarketCard key={data.symbol} data={data} />
-	));
-	const IntMarketCards = internationalMarketData.map((data) => (
-		<MarketCard key={data.symbol} data={data} />
-	));
+	const UsMarketCards = usMarketData.map(marketDataToCard);
+	const IntMarketCards = internationalMarketData.map(marketDataToCard);
 
 	return (
 		<div className="GlobalMarkets">
