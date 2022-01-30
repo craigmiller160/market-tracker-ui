5b81536351c841162c02293fb5fea6796134042b
Craig Miller <craigmiller160@gmail.com>
2022-01-30 12:48:16 -0500

    Stubbed out more tests

--- src/types/tradier/quotes.ts
+++ src/types/tradier/quotes.ts
@@ -1,1 +1,14 @@
-export {};
+export interface Quote {
+	readonly symbol: string;
+	readonly description: string;
+	readonly open: number;
+	readonly high: number;
+	readonly low: number;
+	readonly bid: number;
+	readonly ask: number;
+	readonly close: number;
+}
+
+export interface TradierQuotes {
+	readonly quotes: Quote | ReadonlyArray<Quote>;
+}
--- test/services/TradierService.test.ts
+++ test/services/TradierService.test.ts
@@ -1,7 +1,31 @@
 export {};
 
 describe('TradierService', () => {
-	it('test', () => {
+	it('get single quote', () => {
+		throw new Error();
+	});
+
+	it('get multiple quotes', () => {
+		throw new Error();
+	});
+
+	it('get 1 week history', () => {
+		throw new Error();
+	});
+
+	it('get 1 month history', () => {
+		throw new Error();
+	});
+
+	it('get 3 months history', () => {
+		throw new Error();
+	});
+
+	it('get 1 year history', () => {
+		throw new Error();
+	});
+
+	it('get 5 years history', () => {
 		throw new Error();
 	});
 });
