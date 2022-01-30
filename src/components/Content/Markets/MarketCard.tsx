c2414f5713e1e1d52078cbf38281206dd837233d
Craig Miller <craigmiller160@gmail.com>
2022-01-30 18:04:00 -0500

    More improvements to MarketCard

--- src/components/Content/Markets/MarketCard.tsx
+++ src/components/Content/Markets/MarketCard.tsx
@@ -5,12 +5,17 @@ interface Props {
 	readonly data: MarketData;
 }
 
-export const MarketCard = ({ data }: Props) => {
-	const title = (
+const createTitle = (data: MarketData) => (
+	<>
 		<h3>
 			<strong>{`${data.name} (${data.symbol})`}</strong>
 		</h3>
-	);
+		<p>${data.currentPrice}</p>
+	</>
+);
+
+export const MarketCard = ({ data }: Props) => {
+	const title = createTitle(data);
 	return (
 		<Card title={title} className="MarketCard">
 			<p>The Content</p>
