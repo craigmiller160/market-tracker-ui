3d58b46660af1799285dd6c3e0782c04a3be4c99
Craig Miller <craigmiller160@gmail.com>
2022-01-30 12:20:05 -0500

    Wrote temporary logic to start testing tradier operations

--- src/components/Content/Markets/index.tsx
+++ src/components/Content/Markets/index.tsx
@@ -1,1 +1,18 @@
-export const Markets = () => <h1>Markets Page</h1>;
+import { useEffect } from 'react';
+import { pipe } from 'fp-ts/es6/function';
+import * as tradierService from '../../../services/TradierService';
+import * as TaskEither from 'fp-ts/es6/TaskEither';
+
+export const Markets = () => {
+    // TODO delete this temporary logic
+	useEffect(() => {
+		pipe(
+			tradierService.getQuotes('VTI,VOO'),
+			TaskEither.fold(
+				(ex) => async () => console.error(ex),
+				(data) => async () => console.log(data)
+			)
+		)();
+	}, []);
+	return <h1>Markets Page</h1>;
+};
