53b48088008728fcd26bc1ef4a82306cb9998fba
Craig Miller <craigmiller160@gmail.com>
2022-01-30 16:43:20 -0500

    Beginning work on integrating logic into UI

--- src/components/Content/Markets/index.tsx
+++ src/components/Content/Markets/index.tsx
@@ -1,1 +1,34 @@
-export const Markets = () => <h1>Markets Page</h1>;
+import { useSelector } from 'react-redux';
+import { timeValueSelector } from '../../../store/time/selectors';
+import { useEffect } from 'react';
+import { Updater } from 'use-immer';
+import { HistoryDate } from '../../../types/history';
+import { pipe } from 'fp-ts/es6/function';
+import * as TaskEither from 'fp-ts/es6/TaskEither';
+import * as tradierService from '../../../services/TradierService';
+
+interface MarketData {
+	readonly symbol: string;
+	readonly name: string;
+	readonly currentPrice: number;
+	readonly history: ReadonlyArray<HistoryDate>;
+}
+
+interface State {
+	readonly marketData: ReadonlyArray<MarketData>;
+}
+
+const createLoadMarketData = (setState: Updater<State>) =>
+	pipe(
+		TaskEither.sequenceArray([
+			tradierService.getQuotes(['VTI']),
+			tradierService.getOneWeekHistory('VTI')
+		])
+	);
+
+export const Markets = () => {
+	const timeValue = useSelector(timeValueSelector);
+	useEffect(() => {}, []);
+
+	return <h1>Markets Page</h1>;
+};
