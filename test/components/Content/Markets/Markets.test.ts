09bf032c1821c96ecd3f26f16e764fc2b0a08b75
Craig Miller <craigmiller160@gmail.com>
2022-01-31 16:08:43 -0500

    Stubbing more tests

--- test/components/Content/Markets/Markets.test.ts
+++ test/components/Content/Markets/Markets.test.ts
@@ -1,7 +1,27 @@
 export {};
 
 describe('Markets', () => {
-	it('test', () => {
+	it('renders for today', async () => {
+		throw new Error();
+	});
+
+	it('renders for 1 week', async () => {
+		throw new Error();
+	});
+
+	it('renders for 1 month', async () => {
+		throw new Error();
+	});
+
+	it('renders for 3 months', async () => {
+		throw new Error();
+	});
+
+	it('renders for 1 year', async () => {
+		throw new Error();
+	});
+
+	it('renders for 5 years', async () => {
 		throw new Error();
 	});
 });
