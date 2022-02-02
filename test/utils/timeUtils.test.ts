f84fc969a304eb3e0799aa98e3dcb06586168051
Craig Miller <craigmiller160@gmail.com>
2022-02-02 11:16:29 -0500

    Implemented tests

--- test/utils/timeUtils.test.ts
+++ test/utils/timeUtils.test.ts
@@ -1,25 +1,69 @@
+import {
+	formatHistoryDate,
+	getFiveYearHistoryStartDate,
+	getOneMonthHistoryStartDate,
+	getOneWeekHistoryStartDate,
+	getOneYearHistoryStartDate,
+	getThreeMonthHistoryStartDate,
+	getTodayHistoryStartDate
+} from '../../src/utils/timeUtils';
+import { flow, pipe } from 'fp-ts/es6/function';
+import * as Time from '@craigmiller160/ts-functions/es/Time';
+
 describe('timeUtils', () => {
 	it('getTodayHistoryStartDate', () => {
-		throw new Error();
+		const expected = formatHistoryDate(new Date());
+		const actual = getTodayHistoryStartDate();
+		expect(actual).toEqual(expected);
 	});
 
 	it('getOneWeekHistoryStartDate', () => {
-		throw new Error();
+		const expected = pipe(
+			new Date(),
+			flow(Time.subWeeks(1), Time.addDays(1)),
+			formatHistoryDate
+		);
+		const actual = getOneWeekHistoryStartDate();
+		expect(actual).toEqual(expected);
 	});
 
 	it('getOneMonthHistoryStartDate', () => {
-		throw new Error();
+		const expected = pipe(
+			new Date(),
+			flow(Time.subMonths(1), Time.addDays(1)),
+			formatHistoryDate
+		);
+		const actual = getOneMonthHistoryStartDate();
+		expect(actual).toEqual(expected);
 	});
 
 	it('getThreeMonthHistoryStartDate', () => {
-		throw new Error();
+		const expected = pipe(
+			new Date(),
+			flow(Time.subMonths(3), Time.addDays(1)),
+			formatHistoryDate
+		);
+		const actual = getThreeMonthHistoryStartDate();
+		expect(actual).toEqual(expected);
 	});
 
 	it('getOneYearHistoryStartDate', () => {
-		throw new Error();
+		const expected = pipe(
+			new Date(),
+			flow(Time.subYears(1), Time.addDays(1)),
+			formatHistoryDate
+		);
+		const actual = getOneYearHistoryStartDate();
+		expect(actual).toEqual(expected);
 	});
 
 	it('getFiveYearHistoryStartDate', () => {
-		throw new Error();
+		const expected = pipe(
+			new Date(),
+			flow(Time.subYears(5), Time.addDays(1)),
+			formatHistoryDate
+		);
+		const actual = getFiveYearHistoryStartDate();
+		expect(actual).toEqual(expected);
 	});
 });
