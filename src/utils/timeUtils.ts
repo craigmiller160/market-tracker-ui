b8e8a7a957bdeb549935b3921c3479a5408224d1
Craig Miller <craigmiller160@gmail.com>
2022-02-02 11:12:59 -0500

    Updated timeUtils functions and stubbed out tests for them

--- src/utils/timeUtils.ts
+++ src/utils/timeUtils.ts
@@ -1,17 +1,17 @@
-import { pipe } from 'fp-ts/es6/function';
+import { flow, pipe } from 'fp-ts/es6/function';
 import * as Time from '@craigmiller160/ts-functions/es/Time';
 
 const HISTORY_DATE_FORMAT = 'yyyy-MM-dd';
 export const formatHistoryDate = Time.format(HISTORY_DATE_FORMAT);
 
 const getHistoryStartDate = (intervalFn: (d: Date) => Date): string =>
-	pipe(new Date(), intervalFn, formatHistoryDate);
+	pipe(new Date(), flow(intervalFn, Time.addDays(1)), formatHistoryDate);
 
 export const getTodayHistoryStartDate = (): string =>
 	formatHistoryDate(new Date());
 
 export const getOneWeekHistoryStartDate = (): string =>
-	getHistoryStartDate(Time.subDays(6));
+	getHistoryStartDate(Time.subWeeks(1));
 
 export const getOneMonthHistoryStartDate = (): string =>
 	getHistoryStartDate(Time.subMonths(1));
---
+++ test/utils/timeUtils.test.ts
@@ -0,0 +1,25 @@
+describe('timeUtils', () => {
+	it('getTodayHistoryStartDate', () => {
+		throw new Error();
+	});
+
+	it('getOneWeekHistoryStartDate', () => {
+		throw new Error();
+	});
+
+	it('getOneMonthHistoryStartDate', () => {
+		throw new Error();
+	});
+
+	it('getThreeMonthHistoryStartDate', () => {
+		throw new Error();
+	});
+
+	it('getOneYearHistoryStartDate', () => {
+		throw new Error();
+	});
+
+	it('getFiveYearHistoryStartDate', () => {
+		throw new Error();
+	});
+});
