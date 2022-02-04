1234b686db4c05684ff63a51aa6d89b935694ccf
Craig Miller <craigmiller160@gmail.com>
2022-02-04 16:46:35 -0500

    Experimented with combining the separate ignore patterns config. it's not working right

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
 const merge = require('@craigmiller160/config-merge');
 const jestConfig = require('@craigmiller160/jest-config');
 const jestTsConfig = require('@craigmiller160/jest-config-ts');
+const jestIgnorePatternsConfig = require('@craigmiller160/jest-config-ignore-patterns');
 const path = require('path');
 
-module.exports = merge(jestConfig, jestTsConfig, {
+// TODO does not work with merging the arrays together...
+const config = merge(jestConfig, jestTsConfig, jestIgnorePatternsConfig, {
 	setupFilesAfterEnv: [path.join(process.cwd(), 'test', 'setup.ts')]
 });
+console.log(config.transformIgnorePatterns);
+
+module.exports = config;
