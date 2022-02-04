3d31feca99885f8d2aa49d328d1d23bbef02f47d
Craig Miller <craigmiller160@gmail.com>
2022-02-04 15:50:37 -0500

    Got the chart to work

--- src/components/UI/Chart/index.tsx
+++ src/components/UI/Chart/index.tsx
@@ -1,8 +1,9 @@
 import { Line } from '@ant-design/charts';
 import { MarketData } from '../../../types/MarketData';
 import { useMemo } from 'react';
-import { HistoryRecord } from '../../../types/history';
-import { match, when } from 'ts-pattern';
+import { pipe } from 'fp-ts/es6/function';
+import * as RArray from 'fp-ts/es6/ReadonlyArray';
+import * as Option from 'fp-ts/es6/Option';
 
 interface Props {
 	readonly data: MarketData;
@@ -13,37 +14,32 @@ interface Props {
 // TODO figure out if it's possible to have dedicated data format type
 // TODO need to factor in the time for the timesales setup
 
-const getLastPrice = (
-	history: ReadonlyArray<HistoryRecord>,
-	index: number
-): number =>
-	match(index)
-		.with(
-			when<number>((_) => _ >= 0),
-			() => history[index].price
-		)
-		.otherwise(() => history[index + 1].price);
-
-// TODO need to make everything based on the change from the start, not the change from the last
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const formatData = (data: MarketData): Record<string, any>[] => [
-	...data.history.map((record, index, array) => ({
-		date: `${record.date} ${record.time}`,
-		change: record.price - getLastPrice(array, index - 1)
-	})),
-	{
-		date: 'Now',
-		change:
-			data.currentPrice -
-			getLastPrice(data.history, data.history.length - 1)
-	}
-];
+const formatData = (data: MarketData): ReadonlyArray<Record<string, any>> => {
+	const firstPrice = pipe(
+		RArray.head(data.history),
+		Option.fold(
+			() => 0,
+			(_) => _.price
+		)
+	);
+	return pipe(
+		data.history,
+		RArray.map((record) => ({
+			date: `${record.date} ${record.time}`,
+			change: record.price - firstPrice
+		})),
+		RArray.append({
+			date: 'Now',
+			change: data.currentPrice - firstPrice
+		})
+	);
+};
 
 export const Chart = (props: Props) => {
-	console.log('BeforeData', props.data);
 	const data = useMemo(() => formatData(props.data), [props.data]);
-	console.log('AfterData', data);
+
+	// TODO fix casting
 
 	return (
 		<div>
@@ -52,7 +48,7 @@ export const Chart = (props: Props) => {
 				padding="auto"
 				xField="date"
 				yField="change"
-				data={data}
+				data={data as Record<string, any>[]} // eslint-disable-line
 				color="green"
 			/>
 		</div>
