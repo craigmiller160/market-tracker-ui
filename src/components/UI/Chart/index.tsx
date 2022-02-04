cd38c81832517c0cdd7a953bc80c643cf0d1ba26
Craig Miller <craigmiller160@gmail.com>
2022-02-04 15:59:30 -0500

    Fixed the color of the chart line

--- src/components/UI/Chart/index.tsx
+++ src/components/UI/Chart/index.tsx
@@ -1,10 +1,13 @@
 import { Line } from '@ant-design/charts';
 import { MarketData } from '../../../types/MarketData';
 import { useMemo } from 'react';
-import { pipe } from 'fp-ts/es6/function';
+import { flow, pipe } from 'fp-ts/es6/function';
 import * as RArray from 'fp-ts/es6/ReadonlyArray';
 import * as Option from 'fp-ts/es6/Option';
 import * as Time from '@craigmiller160/ts-functions/es/Time';
+import { HistoryRecord } from '../../../types/history';
+
+// TODO move all data handling logic into custom hook that can be tested separately because of difficulty doing integration tests
 
 const parseTableDate = Time.parse('yyyy-MM-dd HH:mm:ss');
 const formatTableDate = Time.format("M/d/yy'\n'HH:mm");
@@ -13,21 +16,22 @@ interface Props {
 	readonly data: MarketData;
 }
 
-// TODO color changes based on balance being up or down
 // TODO figure out if it's possible to have dedicated data format type
 
 const formatHistoryDate = (tableDate: string): string =>
 	pipe(parseTableDate(tableDate), formatTableDate);
 
+const getFirstPrice: (history: ReadonlyArray<HistoryRecord>) => number = flow(
+	RArray.head,
+	Option.fold(
+		() => 0,
+		(_) => _.price
+	)
+);
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const formatData = (data: MarketData): ReadonlyArray<Record<string, any>> => {
-	const firstPrice = pipe(
-		RArray.head(data.history),
-		Option.fold(
-			() => 0,
-			(_) => _.price
-		)
-	);
+	const firstPrice = getFirstPrice(data.history);
 	return pipe(
 		data.history,
 		RArray.map((record) => ({
@@ -43,6 +47,8 @@ const formatData = (data: MarketData): ReadonlyArray<Record<string, any>> => {
 
 export const Chart = (props: Props) => {
 	const data = useMemo(() => formatData(props.data), [props.data]);
+	const isGain =
+		props.data.currentPrice - getFirstPrice(props.data.history) >= 0;
 
 	// TODO fix casting
 
@@ -54,7 +60,7 @@ export const Chart = (props: Props) => {
 				xField="date"
 				yField="change"
 				data={data as Record<string, any>[]} // eslint-disable-line
-				color="green"
+				color={isGain ? 'green' : 'red'}
 			/>
 		</div>
 	);
