ddc1143b64136a5b25f9d0ad3f94ff647e7f51df
Craig Miller <craigmiller160@gmail.com>
2022-02-04 13:52:42 -0500

    Stubbed out tests

---
+++ test/components/utils/useInterval.test.ts
@@ -0,0 +1,13 @@
+describe('useInterval', () => {
+	it('runs action at interval', async () => {
+		throw new Error();
+	});
+
+	it('changes action while running at interval', async () => {
+		throw new Error();
+	});
+
+	it('changes interval rate', async () => {
+		throw new Error();
+	});
+});
