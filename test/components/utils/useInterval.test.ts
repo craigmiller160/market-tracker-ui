9e89724165d11153c2763ddfe0ad0ba0cfe1f56c
Craig Miller <craigmiller160@gmail.com>
2022-02-04 13:55:13 -0500

    Stopping work on useInterval for now

--- test/components/utils/useInterval.test.ts
+++ test/components/utils/useInterval.test.ts
@@ -1,4 +1,12 @@
+import { useInterval } from '../../../src/components/utils/useInterval';
+
 describe('useInterval', () => {
+	let count = 0;
+	beforeEach(() => {
+		count = 0;
+	});
+	// TODO delete this and the useInterval file if not used
+
 	it('runs action at interval', async () => {
 		throw new Error();
 	});
