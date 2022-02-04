80f5a7e85e8a4e61c7fb026fdc80f7ad33c3711b
Craig Miller <craigmiller160@gmail.com>
2022-02-04 16:53:20 -0500

    Properly preparing to extend transform ignore patterns

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 const merge = require('@craigmiller160/config-merge');
 const jestConfig = require('@craigmiller160/jest-config');
 const jestTsConfig = require('@craigmiller160/jest-config-ts');
-const jestIgnorePatternsConfig = require('@craigmiller160/jest-config-ignore-patterns');
 const path = require('path');
 
-// TODO does not work with merging the arrays together...
-const config = merge(jestConfig, jestTsConfig, jestIgnorePatternsConfig, {
+const config = merge(jestConfig, jestTsConfig, {
 	setupFilesAfterEnv: [path.join(process.cwd(), 'test', 'setup.ts')]
 });
-console.log(config.transformIgnorePatterns);
 
-module.exports = config;
+module.exports = {
+	...config,
+	transformIgnorePatterns: [...config.transformIgnorePatterns.slice(1)]
+};
