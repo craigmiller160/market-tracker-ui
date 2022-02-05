68df37340414e251c5f0bbc9210302f5feb0c07f
Craig Miller <craigmiller160@gmail.com>
2022-02-05 14:55:54 -0500

    More refactoring

--- src/components/Content/Markets/index.tsx
+++ src/components/Content/Markets/index.tsx
@@ -1,16 +1,11 @@
-import { Space, Spin, Typography } from 'antd';
+import { Typography } from 'antd';
 import { useMarketData } from './useMarketData';
 import './Markets.scss';
-import { MarketCard } from './MarketCard';
-import { MarketData } from '../../../types/MarketData';
 import { useContext } from 'react';
 import { ScreenContext } from '../../ScreenContext';
 import { Breakpoints } from '../../utils/Breakpoints';
 import { match } from 'ts-pattern';
-
-// eslint-disable-next-line react/display-name
-const createMarketDataToCard = (timeValue: string) => (data: MarketData) =>
-	<MarketCard key={data.symbol} data={data} time={timeValue} />;
+import { MarketSection } from './MarketSection';
 
 const getPageSizeClass = (breakpoints: Breakpoints): string =>
 	match(breakpoints)
@@ -19,40 +14,28 @@ const getPageSizeClass = (breakpoints: Breakpoints): string =>
 		.with({ xxl: false, xl: false, lg: true }, () => 'LG')
 		.otherwise(() => '');
 
-const Spinner = (
-	<Space size="middle" className="Spinner">
-		<Spin size="large" />
-	</Space>
-);
-
-// TODO split into multiple components
 export const Markets = () => {
 	const { loading, usMarketData, internationalMarketData, timeValue } =
 		useMarketData();
-	const marketDataToCard = createMarketDataToCard(timeValue);
-	const UsMarketCards = usMarketData.map(marketDataToCard);
-	const IntMarketCards = internationalMarketData.map(marketDataToCard);
-	const { breakpoints } = useContext(ScreenContext);
-	const pageSizeClass = getPageSizeClass(breakpoints);
-	const className = `GlobalMarkets ${pageSizeClass}`;
+	// const { breakpoints } = useContext(ScreenContext);
+	// const pageSizeClass = getPageSizeClass(breakpoints);
+	// const className = `GlobalMarkets ${pageSizeClass}`;
 
 	return (
-		<div className={className} data-testid="markets-page">
+		<div className="GlobalMarkets" data-testid="markets-page">
 			<Typography.Title>All Global Markets</Typography.Title>
-			<section className="MarketSection">
-				<Typography.Title level={3}>US Markets</Typography.Title>
-				<div className="MarketCardList" role="list">
-					{loading ? Spinner : UsMarketCards}
-				</div>
-			</section>
-			<section className="MarketSection">
-				<Typography.Title level={3}>
-					International Markets
-				</Typography.Title>
-				<div className="MarketCardList" role="list">
-					{loading ? Spinner : IntMarketCards}
-				</div>
-			</section>
+			<MarketSection
+				title="US Markets"
+				loading={loading}
+				data={usMarketData}
+				timeValue={timeValue}
+			/>
+			<MarketSection
+				title="International Markets"
+				loading={loading}
+				data={internationalMarketData}
+				timeValue={timeValue}
+			/>
 		</div>
 	);
 };
