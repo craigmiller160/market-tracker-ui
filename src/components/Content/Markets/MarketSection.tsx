09f379c3d6ea4ec7d2f5edc26f4a49e12a687944
Craig Miller <craigmiller160@gmail.com>
2022-02-05 15:57:11 -0500

    Feature/mt 51 (#15)
    * Making lots of progress with desktop layout
    
    * Added TODO
    
    * Refactoring SCSS to split up large stylesheet
    
    * Refactored a lot of code into new MarketSection component
    
    * More refactoring
    
    * Changed page title
    
    * Achieved more information-dense view for large screens
    
    * Code cleanup
    
    * Lots of styling improvements
    
    * Fixed broken route
    
    * Added test for routing change
    
    * Added space between market card rows

---
+++ src/components/Content/Markets/MarketSection.tsx
@@ -0,0 +1,36 @@
+import { Space, Spin, Typography } from 'antd';
+import { MarketData } from '../../../types/MarketData';
+import { MarketCard } from './MarketCard';
+import './MarketSection.scss';
+
+interface Props {
+	readonly title: string;
+	readonly loading: boolean;
+	readonly data: ReadonlyArray<MarketData>;
+	readonly timeValue: string;
+}
+
+const Spinner = (
+	<Space size="middle" className="Spinner">
+		<Spin size="large" />
+	</Space>
+);
+
+// eslint-disable-next-line react/display-name
+const createMarketDataToCard = (timeValue: string) => (data: MarketData) =>
+	<MarketCard key={data.symbol} data={data} time={timeValue} />;
+
+export const MarketSection = (props: Props) => {
+	const { title, loading, data, timeValue } = props;
+	const marketDataToCard = createMarketDataToCard(timeValue);
+	const MarketCards = data.map(marketDataToCard);
+
+	return (
+		<section className="MarketSection">
+			<Typography.Title level={3}>{title}</Typography.Title>
+			<div className="MarketCardList" role="list">
+				{loading ? Spinner : MarketCards}
+			</div>
+		</section>
+	);
+};
--- src/components/Content/Markets/index.tsx
+++ src/components/Content/Markets/index.tsx
@@ -1,53 +1,28 @@
-import { Space, Spin, Typography } from 'antd';
+import { Typography } from 'antd';
 import { useMarketData } from './useMarketData';
 import './Markets.scss';
-import { MarketCard } from './MarketCard';
-import { MarketData } from '../../../types/MarketData';
-import { useContext } from 'react';
-import { ScreenContext } from '../../ScreenContext';
-import { Breakpoints } from '../../utils/Breakpoints';
-import { match } from 'ts-pattern';
-
-// eslint-disable-next-line react/display-name
-const createMarketDataToCard = (timeValue: string) => (data: MarketData) =>
-	<MarketCard key={data.symbol} data={data} time={timeValue} />;
-
-const getPageSizeClass = (breakpoints: Breakpoints): string =>
-	match(breakpoints)
-		.with({ xxl: true }, () => 'XXL')
-		.with({ xxl: false, xl: true }, () => 'XL')
-		.with({ xxl: false, xl: false, lg: true }, () => 'LG')
-		.otherwise(() => '');
-
-const Spinner = (
-	<Space size="middle" className="Spinner">
-		<Spin size="large" />
-	</Space>
-);
+import { MarketSection } from './MarketSection';
 
 export const Markets = () => {
 	const { loading, usMarketData, internationalMarketData, timeValue } =
 		useMarketData();
-	const marketDataToCard = createMarketDataToCard(timeValue);
-	const UsMarketCards = usMarketData.map(marketDataToCard);
-	const IntMarketCards = internationalMarketData.map(marketDataToCard);
-	const { breakpoints } = useContext(ScreenContext);
-	const pageSizeClass = getPageSizeClass(breakpoints);
-	const className = `GlobalMarkets ${pageSizeClass}`;
 
 	return (
-		<div className={className} data-testid="markets-page">
-			<Typography.Title>All Global Markets</Typography.Title>
-			<section className="MarketSection">
-				<Typography.Title level={3}>US Markets</Typography.Title>
-				{loading ? Spinner : UsMarketCards}
-			</section>
-			<section className="MarketSection">
-				<Typography.Title level={3}>
-					International Markets
-				</Typography.Title>
-				{loading ? Spinner : IntMarketCards}
-			</section>
+		<div className="GlobalMarkets" data-testid="markets-page">
+			<Typography.Title>All Markets</Typography.Title>
+			<MarketSection
+				title="US Markets"
+				loading={loading}
+				data={usMarketData}
+				timeValue={timeValue}
+			/>
+			<hr />
+			<MarketSection
+				title="International Markets"
+				loading={loading}
+				data={internationalMarketData}
+				timeValue={timeValue}
+			/>
 		</div>
 	);
 };
