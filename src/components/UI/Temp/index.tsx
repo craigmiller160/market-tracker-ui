93bba47253b7b8a47737858ead7c6df74a270e9b
Craig Miller <craigmiller160@gmail.com>
2022-02-06 11:09:42 -0500

    My prototype notification component is working

--- src/components/UI/Temp/index.tsx
+++ src/components/UI/Temp/index.tsx
@@ -3,7 +3,7 @@ import { alertSelector } from '../../../store/alert/selectors';
 import { match } from 'ts-pattern';
 import { useEffect } from 'react';
 import { alertSlice } from '../../../store/alert/slice';
-import { notification } from 'antd';
+import { notification as antNotification } from 'antd';
 import { notificationSlice } from '../../../store/notification/slice';
 import { notificationsSelector } from '../../../store/notification/selectors';
 
@@ -14,29 +14,29 @@ export const Temp = () => {
 	const notifications = useSelector(notificationsSelector, shallowEqual);
 
 	useEffect(() => {
-
+		notifications.forEach((notification) => {
+			if (!notification.isShown) {
+				antNotification[notification.type]({
+					message: notification.message,
+					description: notification.description,
+					onClose: () => {
+						dispatch(
+							notificationSlice.actions.hide(notification.id)
+						);
+					}
+				});
+				dispatch(notificationSlice.actions.markShown(notification.id));
+			}
+		});
 	}, [notifications.length]);
-
-
-	// useEffect(() => {
-	// 	if (show) {
-	// 		notification[type]({
-	// 			message: topMessage,
-	// 			description: message,
-	// 			onClose: () => {
-	// 				dispatch(notificationSlice.actions.hide());
-	// 			}
-	// 		});
-	// 	}
-	// }, [show]);
 	return (
 		<div>
 			<button
 				onClick={() =>
-					dispatch(notificationSlice.actions.showError('Hello World'))
+					dispatch(notificationSlice.actions.addError('Hello World'))
 				}
 			>
-				Show Notification
+				Add Notification
 			</button>
 		</div>
 	);
