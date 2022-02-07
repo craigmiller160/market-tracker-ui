630105036605fd31514510c69b65c54fda81b829
Craig Miller <craigmiller160@gmail.com>
2022-02-06 21:52:22 -0500

    Even more refactoring for MarketTime enum

--- test/store/time/slice.test.ts
+++ test/store/time/slice.test.ts
@@ -5,11 +5,13 @@ describe('time slice', () => {
 	it('setTime', () => {
 		const result = timeSlice.reducer(
 			timeSlice.getInitialState(),
-			timeSlice.actions.setTime('time.fiveYears')
+			timeSlice.actions.setTime(
+				marketTimeToMenuKey(MarketTime.FIVE_YEARS)
+			)
 		);
 		expect(result).toEqual({
-			menuKey: 'time.fiveYears',
-			value: 'fiveYears'
+			menuKey: marketTimeToMenuKey(MarketTime.FIVE_YEARS),
+			value: MarketTime.FIVE_YEARS
 		});
 	});
 
