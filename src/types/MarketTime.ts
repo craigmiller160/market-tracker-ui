202c4537c04765e1da05c4512178c232e3a17b42
Craig Miller <craigmiller160@gmail.com>
2022-02-06 21:16:48 -0500

    Added MarketTime enum

---
+++ src/types/MarketTime.ts
@@ -0,0 +1,8 @@
+export enum MarketTime {
+	ONE_DAY = 'oneDay',
+	ONE_WEEK = 'oneWeek',
+	ONE_MONTH = 'oneMonth',
+	THREE_MONTHS = 'threeMonths',
+	ONE_YEAR = 'oneYear',
+	FIVE_YEARS = 'fiveYears'
+}
