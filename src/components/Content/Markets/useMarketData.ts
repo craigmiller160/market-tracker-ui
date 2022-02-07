9b232b7cb7d4b2ddea63286555d7e7560fdd70d4
Craig Miller <craigmiller160@gmail.com>
2022-02-07 11:52:11 -0500

    Added TODO

--- src/components/Content/Markets/useMarketData.ts
+++ src/components/Content/Markets/useMarketData.ts
@@ -14,6 +14,7 @@ import { Dispatch } from 'redux';
 import { TaskT } from '@craigmiller160/ts-functions/es/types';
 
 const INTERVAL_1_MIN_MILLIS = 1000 * 60;
+// TODO when repeating, the old call may still exist and needs to be aborted before setting the values
 
 interface State {
 	readonly loading: boolean;
