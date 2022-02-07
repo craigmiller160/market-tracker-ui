5b098c8720ae3b78107f5b386a7a23d7c57a53f4
Craig Miller <craigmiller160@gmail.com>
2022-02-07 16:40:25 -0500

    Fixed lint stuff in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,42 @@
 const { merge } = require('webpack-merge');
 const config = require('@craigmiller160/webpack-config');
-const { isDevelopment } = require('@craigmiller160/webpack-config/utils/nodeEnvCheck')
+const {
+	isDevelopment
+} = require('@craigmiller160/webpack-config/utils/nodeEnvCheck');
 const sassConfig = require('@craigmiller160/webpack-config-sass');
 const tsConfig = require('@craigmiller160/webpack-config-ts');
 
 const localDevServerConfig = {
-    devServer: {
-        port: 3000,
-        https: true,
-        proxy: {
-            '/market-tracker/api': {
-                target: 'https://localhost:8080',
-                changeOrigin: true,
-                secure: false,
-                pathRewrite: {
-                    '^/market-tracker/api': ''
-                },
-                logLevel: 'debug'
-            },
-            '/market-tracker/oauth2': {
-                target: 'https://localhost:7003',
-                changeOrigin: true,
-                secure: false,
-                pathRewrite: {
-                    '^/market-tracker/oauth2': ''
-                },
-                logLevel: 'debug'
-            }
-        }
-    }
+	devServer: {
+		port: 3000,
+		https: true,
+		proxy: {
+			'/market-tracker/api': {
+				target: 'https://localhost:8080',
+				changeOrigin: true,
+				secure: false,
+				pathRewrite: {
+					'^/market-tracker/api': ''
+				},
+				logLevel: 'debug'
+			},
+			'/market-tracker/oauth2': {
+				target: 'https://localhost:7003',
+				changeOrigin: true,
+				secure: false,
+				pathRewrite: {
+					'^/market-tracker/oauth2': ''
+				},
+				logLevel: 'debug'
+			}
+		}
+	}
 };
 
 const parts = [config, sassConfig, tsConfig];
 
 if (isDevelopment()) {
-    parts.push(localDevServerConfig);
+	parts.push(localDevServerConfig);
 }
 
 module.exports = merge(parts);
