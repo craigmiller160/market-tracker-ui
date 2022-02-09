7faac700772d9c5cd925d91ac9a1f45da327281d
Craig Miller <craigmiller160@gmail.com>
2022-02-08 19:35:01 -0500

    Got all logos added

--- src/components/Content/Recognition/index.tsx
+++ src/components/Content/Recognition/index.tsx
@@ -1,13 +1,29 @@
 import './Recognition.scss';
 import { Typography } from 'antd';
 import TradierLogo from '../../../images/tradier.png';
+import MessariLogo from '../../../images/messari.png';
+import { useContext } from 'react';
+import { ScreenContext } from '../../ScreenContext';
+import { getBreakpointName } from '../../utils/Breakpoints';
 
-export const Recognition = () => (
-	<div className="Recognition">
-		<Typography.Title>Data Source Recognition</Typography.Title>
-		<img src={TradierLogo} alt="Tradier" />
-		<Typography.Title level={4}>
-			The source of all stock market data
-		</Typography.Title>
-	</div>
-);
+export const Recognition = () => {
+	const { breakpoints } = useContext(ScreenContext);
+	const breakpointName = getBreakpointName(breakpoints);
+	return (
+		<div className="Recognition">
+			<Typography.Title>Data Source Recognition</Typography.Title>
+			<div className={`Source ${breakpointName}`}>
+				<img src={TradierLogo} alt="Tradier" />
+				<Typography.Title level={4}>
+					The source of all stock market data
+				</Typography.Title>
+			</div>
+			<div className={`Source ${breakpointName}`}>
+				<img src={MessariLogo} alt="Messari" />
+				<Typography.Title level={4}>
+					The source of all crypto data
+				</Typography.Title>
+			</div>
+		</div>
+	);
+};
