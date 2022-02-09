acba976654f46d4ef7855f6f06142376cf9c4691
Craig Miller <craigmiller160@gmail.com>
2022-02-08 19:28:24 -0500

    Got the image to work and decided on a layout

--- src/components/Content/Recognition/index.tsx
+++ src/components/Content/Recognition/index.tsx
@@ -1,16 +1,13 @@
 import './Recognition.scss';
 import { Typography } from 'antd';
+import TradierLogo from '../../../images/tradier.png';
 
 export const Recognition = () => (
 	<div className="Recognition">
 		<Typography.Title>Data Source Recognition</Typography.Title>
-		<div className="Sources">
-			<div className="Source">
-				<a href="https://documentation.tradier.com/brokerage-api/overview/market-data">
-					<Typography.Title level={4}>Tradier</Typography.Title>
-				</a>
-				<Typography.Title level={4}>The source of all</Typography.Title>
-			</div>
-		</div>
+		<img src={TradierLogo} alt="Tradier" />
+		<Typography.Title level={4}>
+			The source of all stock market data
+		</Typography.Title>
 	</div>
 );
