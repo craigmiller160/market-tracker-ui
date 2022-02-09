c2074f48273ac178c8480990a4f4d06490ebcefc
Craig Miller <craigmiller160@gmail.com>
2022-02-08 19:20:28 -0500

    Trying to figure out what the sources page should look like

--- src/components/Content/Recognition/index.tsx
+++ src/components/Content/Recognition/index.tsx
@@ -4,5 +4,13 @@ import { Typography } from 'antd';
 export const Recognition = () => (
 	<div className="Recognition">
 		<Typography.Title>Data Source Recognition</Typography.Title>
+		<div className="Sources">
+			<div className="Source">
+				<a href="https://documentation.tradier.com/brokerage-api/overview/market-data">
+					<Typography.Title level={4}>Tradier</Typography.Title>
+				</a>
+				<Typography.Title level={4}>The source of all</Typography.Title>
+			</div>
+		</div>
 	</div>
 );
