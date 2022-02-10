acd74f8a7872ac13dc9248e5a9631750e7466adf
Craig Miller <craigmiller160@gmail.com>
2022-02-10 16:27:36 -0500

    Stubbed out tests

---
+++ test/services/CoinGeckoService.test.ts
@@ -0,0 +1,11 @@
+export {};
+
+describe('CoinGeckoService', () => {
+	it('getQuotes', async () => {
+		throw new Error();
+	});
+
+	it('getHistoryQuote', async () => {
+		throw new Error();
+	});
+});
