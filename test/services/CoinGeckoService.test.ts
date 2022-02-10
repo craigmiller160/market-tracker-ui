8b2ff240e53ccef213bf84d904b36aabd7765a58
Craig Miller <craigmiller160@gmail.com>
2022-02-10 17:02:04 -0500

    Implemented a test

--- test/services/CoinGeckoService.test.ts
+++ test/services/CoinGeckoService.test.ts
@@ -1,8 +1,47 @@
-export {};
+import { CoinGeckoPrice } from '../../src/types/coingecko/CoinGeckoPrice';
+import { ajaxApi } from '../../src/services/AjaxApi';
+import MockAdapter from 'axios-mock-adapter';
+import '@relmify/jest-fp-ts';
+import { getQuotes } from '../../src/services/CoinGeckoService';
+
+const symbols: ReadonlyArray<string> = ['bitcoin', 'ethereum'];
+
+const price: CoinGeckoPrice = {
+	bitcoin: {
+		usd: '100.50'
+	},
+	ethereum: {
+		usd: '50.75'
+	}
+};
+
+const mockApi = new MockAdapter(ajaxApi.instance);
 
 describe('CoinGeckoService', () => {
+	beforeEach(() => {
+		mockApi.reset();
+	});
+
 	it('getQuotes', async () => {
-		throw new Error();
+		mockApi
+			.onGet(
+				`/coingecko/simple/price?ids=${symbols.join(
+					','
+				)}&vs_currencies=usd`
+			)
+			.reply(200, price);
+
+		const result = await getQuotes(symbols)();
+		expect(result).toEqualRight([
+			{
+				symbol: 'bitcoin',
+				price: 100.5
+			},
+			{
+				symbol: 'ethereum',
+				price: 50.75
+			}
+		]);
 	});
 
 	it('getHistoryQuote', async () => {
