ac6bbfdeda264de420ec77271264341aec4c80fc
Craig Miller <craigmiller160@gmail.com>
2022-02-13 08:37:26 -0500

    Fixed formatting of numbers on cards

--- src/components/Content/Markets/MarketCard.tsx
+++ src/components/Content/Markets/MarketCard.tsx
@@ -31,18 +31,27 @@ const createTitle = (data: MarketData): ReactNode => (
 	</h3>
 );
 
+const localeOptions: Intl.NumberFormatOptions = {
+	minimumFractionDigits: 2,
+	maximumFractionDigits: 2
+};
+
 const createPrice = (data: MarketData) => {
 	const oldestPrice = data.history[0]?.price ?? 0;
 	const priceChange = data.currentPrice - oldestPrice;
 
-	const formattedPrice = `$${data.currentPrice.toFixed(2)}`;
+	const formattedPrice = `$${data.currentPrice.toLocaleString(
+		undefined,
+		localeOptions
+	)}`;
 	const priceChangeOperator = priceChange >= 0 ? '+' : '-';
 	const formattedPriceChange = `${priceChangeOperator}$${Math.abs(
 		priceChange
-	).toFixed(2)}`;
+	).toLocaleString(undefined, localeOptions)}`;
 	const percentChange = (Math.abs(priceChange) / oldestPrice) * 100;
-	const formattedPercentChange = `${priceChangeOperator}${percentChange.toFixed(
-		2
+	const formattedPercentChange = `${priceChangeOperator}${percentChange.toLocaleString(
+		undefined,
+		localeOptions
 	)}%`;
 	const priceClassName = priceChange >= 0 ? 'up' : 'down';
 	const ChangeIcon =
