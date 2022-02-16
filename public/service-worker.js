06c9f871c357625088ef810a1d0538ad43163c55
Craig Miller <craigmiller160@gmail.com>
2022-02-16 16:34:21 -0500

    Trying and failing to get service worker to work

---
+++ public/service-worker.js
@@ -0,0 +1,3 @@
+// TODO figure out a better place to put this, and also TS support
+
+console.log('Hello Service Worker');
--- src/index.tsx
+++ src/index.tsx
@@ -6,3 +6,10 @@ import './index.scss';
 import { App } from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker
+		.register('/service-worker.js')
+		.then(() => console.log('Worker registered'))
+		.catch((ex) => console.error('Worker registration error', ex));
+}
