6a3c9b72bdfda6d51bec48d619226e7e1a67af45
Craig Miller <craigmiller160@gmail.com>
2022-02-16 14:12:25 -0500

    Even more test setup work

--- test/services/AjaxApi.Error.test.ts
+++ test/services/AjaxApi.Error.test.ts
@@ -1,15 +1,26 @@
 import { ajaxApi } from '../../src/services/AjaxApi';
 import MockAdapter from 'axios-mock-adapter';
+import { store } from '../../src/store';
+import { MockStore } from 'redux-mock-store';
+
+jest.mock('../../src/store', () => {
+	const createMockStore = jest.requireActual('redux-mock-store');
+	return {
+		store: createMockStore()
+	};
+});
 
 const mockApi = new MockAdapter(ajaxApi.instance);
+const mockStore = store as MockStore;
 
 describe('AjaxApi Error Handler', () => {
 	beforeEach(() => {
 		mockApi.reset();
+		mockStore.clearActions();
 	});
 
 	it('is 500 error, no body', () => {
-		throw new Error();
+		mockApi.onGet('/foo').reply(500);
 	});
 
 	it('is 401 error, no body', () => {
