30f1009fdc1a99596dda80cfbc36070039b993ee
Craig Miller <craigmiller160@gmail.com>
2022-02-17 16:15:14 -0500

    Slowly getting serviceworker operational, in this case fixed typescript type errors

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,3 +1,11 @@
+/// <reference no-default-lib="true"/>
+/// <reference lib="ES2015" />
+/// <reference lib="webworker" />
+
 export {};
 
-// console.log('FooBar');
+declare const self: ServiceWorkerGlobalScope;
+
+self.addEventListener('install', () => {
+	self.skipWaiting();
+});
