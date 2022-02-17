1bb5019bd30da6dcb2d00848e8c04cd3ccbf1516
Craig Miller <craigmiller160@gmail.com>
2022-02-17 13:59:11 -0500

    Added service worker registration

--- src/index.tsx
+++ src/index.tsx
@@ -6,3 +6,10 @@ import './index.scss';
 import { App } from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker
+		.register('/service-worker.js')
+		.then(() => console.log('Service Worker Registered successfully'))
+		.catch((ex) => console.error('Service Worker Registration failed', ex));
+}
