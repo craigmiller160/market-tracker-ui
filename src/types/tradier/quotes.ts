ec3e4c731f2a2caa583bc0a3fb9963fdacaa7cf3
Craig Miller <craigmiller160@gmail.com>
2022-02-18 14:48:32 -0500

    More cleanup of readonly constraints

--- src/types/tradier/quotes.ts
+++ src/types/tradier/quotes.ts
@@ -1,27 +1,30 @@
-// TODO fix readonly
 import * as ioType from 'io-ts';
 
-export const tradierQuoteV = ioType.type({
-	symbol: ioType.readonly(ioType.string),
-	description: ioType.readonly(ioType.string),
-	open: ioType.readonly(ioType.number),
-	high: ioType.readonly(ioType.number),
-	low: ioType.readonly(ioType.number),
-	bid: ioType.readonly(ioType.number),
-	ask: ioType.readonly(ioType.number),
-	close: ioType.union([ioType.number, ioType.null]),
-	last: ioType.readonly(ioType.number)
-});
+export const tradierQuoteV = ioType.readonly(
+	ioType.type({
+		symbol: ioType.string,
+		description: ioType.string,
+		open: ioType.number,
+		high: ioType.number,
+		low: ioType.number,
+		bid: ioType.number,
+		ask: ioType.number,
+		close: ioType.union([ioType.number, ioType.null]),
+		last: ioType.number
+	})
+);
 export type TradierQuote = ioType.TypeOf<typeof tradierQuoteV>;
 
-export const tradierQuotesV = ioType.type({
-	quotes: ioType.readonly(
-		ioType.type({
-			quote: ioType.union([
-				tradierQuoteV,
-				ioType.readonlyArray(tradierQuoteV)
-			])
-		})
-	)
-});
+export const tradierQuotesV = ioType.readonly(
+	ioType.type({
+		quotes: ioType.readonly(
+			ioType.type({
+				quote: ioType.union([
+					tradierQuoteV,
+					ioType.readonlyArray(tradierQuoteV)
+				])
+			})
+		)
+	})
+);
 export type TradierQuotes = ioType.TypeOf<typeof tradierQuotesV>;
