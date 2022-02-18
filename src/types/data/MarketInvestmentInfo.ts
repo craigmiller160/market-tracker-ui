2e60d3cd4cff83e96e35f5456b91c1062579e089
Craig Miller <craigmiller160@gmail.com>
2022-02-18 08:54:46 -0500

    Working on io-ts driven types for JSON data

---
+++ src/types/data/MarketInvestmentInfo.ts
@@ -0,0 +1,32 @@
+import * as ioType from 'io-ts';
+
+export enum MarketInvestmentType {
+	USA_ETF = 'USA_ETF',
+	INTERNATIONAL_ETF = 'INTERNATIONAL_ETF',
+	CRYPTO = 'CRYPTO'
+}
+
+const marketInvestmentTypeV = ioType.keyof({
+	[MarketInvestmentType.USA_ETF]: null,
+	[MarketInvestmentType.INTERNATIONAL_ETF]: null,
+	[MarketInvestmentType.CRYPTO]: null
+});
+
+const marketInvestmentInfoV = ioType.type({
+	symbol: ioType.readonly(ioType.string),
+	name: ioType.readonly(ioType.string),
+	type: ioType.readonly(marketInvestmentTypeV)
+});
+export type MarketInvestmentInfo = ioType.TypeOf<typeof marketInvestmentInfoV>;
+
+// TODO delete this
+const foo: MarketInvestmentInfo = {
+	symbol: '',
+	name: '',
+	type: MarketInvestmentType.USA_ETF
+};
+
+const marketInvestmentInfoArrayV = ioType.readonlyArray(marketInvestmentInfoV);
+export type MarketInvestmentInfoArray = ioType.TypeOf<
+	typeof marketInvestmentInfoArrayV
+>;
