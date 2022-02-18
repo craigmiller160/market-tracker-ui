3c32444438fdb779f7aa50e690134ce760a95414
Craig Miller <craigmiller160@gmail.com>
2022-02-18 09:25:36 -0500

    Working on logic to handle parsing/mapping data

--- src/data/MarketPageInvestmentParsing.ts
+++ src/data/MarketPageInvestmentParsing.ts
@@ -1,14 +1,59 @@
 import marketDataJson from './marketsPageInvestments.json';
 import {
+	MarketInvestmentInfo,
 	MarketInvestmentInfoArray,
-	marketInvestmentInfoArrayV
+	marketInvestmentInfoArrayV,
+	MarketInvestmentType
 } from '../types/data/MarketInvestmentInfo';
 import { pipe } from 'fp-ts/es6/function';
 import { handleValidationResult } from '../errors/TypeValidationError';
+import { MonoidT } from '@craigmiller160/ts-functions/es/types';
+import * as Monoid from 'fp-ts/es6/Monoid';
+import * as RArray from 'fp-ts/es6/ReadonlyArray';
+
+type InvestmentsByType = {
+	[key in MarketInvestmentType]: MarketInvestmentInfoArray;
+};
 
 export const getMarketInvestmentInfo = () => {
 	const result = pipe(
 		marketInvestmentInfoArrayV.decode(marketDataJson),
 		handleValidationResult
+	);
+};
+
+const groupByTypeMonoid: MonoidT<InvestmentsByType> = {
+	empty: {
+		[MarketInvestmentType.USA_ETF]: [],
+		[MarketInvestmentType.INTERNATIONAL_ETF]: [],
+		[MarketInvestmentType.CRYPTO]: []
+	},
+	concat: (invest1, invest2) => ({
+		[MarketInvestmentType.USA_ETF]: invest1[
+			MarketInvestmentType.USA_ETF
+		].concat(invest2[MarketInvestmentType.USA_ETF]),
+		[MarketInvestmentType.INTERNATIONAL_ETF]: invest1[
+			MarketInvestmentType.INTERNATIONAL_ETF
+		].concat(invest2[MarketInvestmentType.INTERNATIONAL_ETF]),
+		[MarketInvestmentType.CRYPTO]: invest1[
+			MarketInvestmentType.CRYPTO
+		].concat(invest2[MarketInvestmentType.CRYPTO])
+	})
+};
+
+const infoToInvestmentByType = (
+	info: MarketInvestmentInfo
+): InvestmentsByType => {
+	// TODO finish this
+	throw new Error();
+};
+
+const groupInvestmentsByType = (
+	infoArray: MarketInvestmentInfoArray
+): InvestmentsByType => {
+	const result = pipe(
+		infoArray,
+		RArray.map(infoToInvestmentByType),
+		Monoid.concatAll(groupByTypeMonoid)
 	);
 };
