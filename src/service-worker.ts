37f3b5f783336f0bae2109357e240f59e005d0f8
Craig Miller <craigmiller160@gmail.com>
2022-02-19 10:25:06 -0500

    Fully implemented the service worker offline caching

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -9,6 +9,9 @@ self.__WB_MANIFEST;
 const MARKET_DATA_CACHE = 'market-data-cache';
 const MARKET_DATA_REGEX = /^.*\/api\/(tradier|coingecko)\/.*$/;
 
+const isCacheableStatus = (response: Response): boolean =>
+	response.status >= 200 && response.status <= 300;
+
 self.addEventListener('install', (event) => {
 	event.waitUntil(self.skipWaiting());
 });
@@ -21,20 +24,36 @@ self.addEventListener('fetch', (event) => {
 	event.respondWith(
 		fetch(event.request)
 			.then((response) => {
-				if (MARKET_DATA_REGEX.test(event.request.url)) {
-					console.log('Response', response);
+				if (
+					MARKET_DATA_REGEX.test(event.request.url) &&
+					isCacheableStatus(response)
+				) {
+					caches
+						.open(MARKET_DATA_CACHE)
+						.then((cache) =>
+							cache.put(event.request, response.clone())
+						);
 				}
 				return response;
 			})
 			.catch((ex) => {
-				console.log(
-					'WorkerError',
-					ex.message,
-					Object.keys(ex),
-					navigator.onLine
-				);
-				const init = { status: 500, statusText: 'offline' };
-				return new Response('', init);
+				if (!navigator.onLine) {
+					return caches
+						.open(MARKET_DATA_CACHE)
+						.then((cache) => cache.match(event.request))
+						.then(
+							(response) =>
+								response ??
+								new Response('Offline', {
+									status: 500,
+									statusText: 'offline'
+								})
+						);
+				}
+
+				console.error('Critical Error', ex);
+				const init = { status: 500, statusText: 'Critical Error' };
+				return new Response('Critical Error', init);
 			})
 	);
 });
