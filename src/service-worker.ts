9c3ed3e2fd1f226453d47797a76ed95f998df85f
Craig Miller <craigmiller160@gmail.com>
2022-02-19 10:20:11 -0500

    More service worker cleanup

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -9,21 +9,12 @@ self.__WB_MANIFEST;
 const MARKET_DATA_CACHE = 'market-data-cache';
 const MARKET_DATA_REGEX = /^.*\/api\/(tradier|coingecko)\/.*$/;
 
-self.addEventListener('install', () => {
-	console.log('Installed');
-	self.skipWaiting().then(() => console.log('Skipped Waiting'));
+self.addEventListener('install', (event) => {
+	event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener('activate', (event) => {
-	console.log('Activated');
-	event.waitUntil(
-		self.clients
-			.claim()
-			.then(() => caches.keys())
-			.then((cacheNames: string[]) =>
-				Promise.all(cacheNames.map((name) => caches.delete(name)))
-			)
-	);
+	event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
@@ -36,7 +27,12 @@ self.addEventListener('fetch', (event) => {
 				return response;
 			})
 			.catch((ex) => {
-				console.log('WorkerError', ex.message, Object.keys(ex), navigator.onLine);
+				console.log(
+					'WorkerError',
+					ex.message,
+					Object.keys(ex),
+					navigator.onLine
+				);
 				const init = { status: 500, statusText: 'offline' };
 				return new Response('', init);
 			})
