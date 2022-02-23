1bdd200151422ce5eab2928dfcbeb1b4e771364a
Craig Miller <craigmiller160@gmail.com>
2022-02-23 13:59:59 -0500

    Implemented history function tests

--- test/services/MarketInvestmentService.test.ts
+++ test/services/MarketInvestmentService.test.ts
@@ -1,55 +1,137 @@
-export {};
+import { getHistoryFn } from '../../src/services/MarketInvestmentService';
+import { MarketTime } from '../../src/types/MarketTime';
+import { MarketInvestmentType } from '../../src/types/data/MarketInvestmentType';
+import * as tradierService from '../../src/services/TradierService';
+import * as coinGeckoService from '../../src/services/CoinGeckoService';
 
 // TODO take the API setup stuff for the regexes in Markets.test and make it re-usable
 
 describe('MarketInvestmentService', () => {
 	describe('getHistoryFn', () => {
 		it('Tradier One Day', () => {
-			throw new Error();
+			const usResult = getHistoryFn(
+				MarketTime.ONE_DAY,
+				MarketInvestmentType.USA_ETF
+			);
+			const intResult = getHistoryFn(
+				MarketTime.ONE_DAY,
+				MarketInvestmentType.INTERNATIONAL_ETF
+			);
+			expect(usResult).toEqual(tradierService.getTimesales);
+			expect(intResult).toEqual(tradierService.getTimesales);
 		});
 
 		it('CoinGecko One Day', () => {
-			throw new Error();
+			const result = getHistoryFn(
+				MarketTime.ONE_DAY,
+				MarketInvestmentType.CRYPTO
+			);
+			expect(result).toEqual(coinGeckoService.getTodayHistory);
 		});
 
 		it('Tradier One Week', () => {
-			throw new Error();
+			const usResult = getHistoryFn(
+				MarketTime.ONE_WEEK,
+				MarketInvestmentType.USA_ETF
+			);
+			const intResult = getHistoryFn(
+				MarketTime.ONE_WEEK,
+				MarketInvestmentType.INTERNATIONAL_ETF
+			);
+			expect(usResult).toEqual(tradierService.getOneWeekHistory);
+			expect(intResult).toEqual(tradierService.getOneWeekHistory);
 		});
 
 		it('CoinGecko One Week', () => {
-			throw new Error();
+			const result = getHistoryFn(
+				MarketTime.ONE_WEEK,
+				MarketInvestmentType.CRYPTO
+			);
+			expect(result).toEqual(coinGeckoService.getOneWeekHistory);
 		});
 
 		it('Tradier One Month', () => {
-			throw new Error();
+			const usResult = getHistoryFn(
+				MarketTime.ONE_MONTH,
+				MarketInvestmentType.USA_ETF
+			);
+			const intResult = getHistoryFn(
+				MarketTime.ONE_MONTH,
+				MarketInvestmentType.INTERNATIONAL_ETF
+			);
+			expect(usResult).toEqual(tradierService.getOneMonthHistory);
+			expect(intResult).toEqual(tradierService.getOneMonthHistory);
 		});
 
 		it('CoinGecko One Month', () => {
-			throw new Error();
+			const result = getHistoryFn(
+				MarketTime.ONE_MONTH,
+				MarketInvestmentType.CRYPTO
+			);
+			expect(result).toEqual(coinGeckoService.getOneMonthHistory);
 		});
 
 		it('Tradier Three Months', () => {
-			throw new Error();
+			const usResult = getHistoryFn(
+				MarketTime.THREE_MONTHS,
+				MarketInvestmentType.USA_ETF
+			);
+			const intResult = getHistoryFn(
+				MarketTime.THREE_MONTHS,
+				MarketInvestmentType.INTERNATIONAL_ETF
+			);
+			expect(usResult).toEqual(tradierService.getThreeMonthHistory);
+			expect(intResult).toEqual(tradierService.getThreeMonthHistory);
 		});
 
 		it('CoinGecko Three Months', () => {
-			throw new Error();
+			const result = getHistoryFn(
+				MarketTime.THREE_MONTHS,
+				MarketInvestmentType.CRYPTO
+			);
+			expect(result).toEqual(coinGeckoService.getThreeMonthHistory);
 		});
 
 		it('Tradier One Year', () => {
-			throw new Error();
+			const usResult = getHistoryFn(
+				MarketTime.ONE_YEAR,
+				MarketInvestmentType.USA_ETF
+			);
+			const intResult = getHistoryFn(
+				MarketTime.ONE_YEAR,
+				MarketInvestmentType.INTERNATIONAL_ETF
+			);
+			expect(usResult).toEqual(tradierService.getOneYearHistory);
+			expect(intResult).toEqual(tradierService.getOneYearHistory);
 		});
 
 		it('CoinGecko One Year', () => {
-			throw new Error();
+			const result = getHistoryFn(
+				MarketTime.ONE_YEAR,
+				MarketInvestmentType.CRYPTO
+			);
+			expect(result).toEqual(coinGeckoService.getOneYearHistory);
 		});
 
 		it('Tradier Five Years', () => {
-			throw new Error();
+			const usResult = getHistoryFn(
+				MarketTime.FIVE_YEARS,
+				MarketInvestmentType.USA_ETF
+			);
+			const intResult = getHistoryFn(
+				MarketTime.FIVE_YEARS,
+				MarketInvestmentType.INTERNATIONAL_ETF
+			);
+			expect(usResult).toEqual(tradierService.getFiveYearHistory);
+			expect(intResult).toEqual(tradierService.getFiveYearHistory);
 		});
 
 		it('CoinGecko Five Years', () => {
-			throw new Error();
+			const result = getHistoryFn(
+				MarketTime.FIVE_YEARS,
+				MarketInvestmentType.CRYPTO
+			);
+			expect(result).toEqual(coinGeckoService.getFiveYearHistory);
 		});
 	});
 
