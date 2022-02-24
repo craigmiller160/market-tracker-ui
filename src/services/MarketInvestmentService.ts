d09397f6523674a121cc1fa63a0726c85f247a28
Craig Miller <craigmiller160@gmail.com>
2022-02-24 12:00:05 -0500

    Fixed startPrice bug

--- src/services/MarketInvestmentService.ts
+++ src/services/MarketInvestmentService.ts
@@ -211,10 +211,12 @@ const handleUseQuoteOrHistoryQuote =
 const handleInvestmentData =
 	(time: MarketTime) =>
 	({ history, quote }: IntermediateInvestmentData): InvestmentData => {
+		// TODO add new startPrice test if this works
 		const startPrice =
-			quote.previousClose > 0 ? quote.previousClose : history?.[0]?.price;
+			quote.previousClose > 0 && MarketTime.ONE_DAY === time
+				? quote.previousClose
+				: history?.[0]?.price;
 
-		// TODO no need for extra history record when already using history for start price
 		const newHistory = match({ time, startPrice })
 			.with(
 				{ time: MarketTime.ONE_DAY, startPrice: quote.previousClose },
