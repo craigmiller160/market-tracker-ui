e9c751633d152307f5852786d6ba3f4a202c8244
Craig Miller <craigmiller160@gmail.com>
2022-02-24 15:57:56 -0500

    Got https working for vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
 export default {
 	root: path.join(process.cwd(), 'src'),
+	base: '/market-tracker',
 	publicDir: path.join(process.cwd(), 'public'),
 	server: {
-		port: 3000
+		port: 3000,
+		https: {
+			cert: fs.readFileSync(
+				path.join(process.cwd(), 'config', 'localhost.cert.pem'),
+				'utf8'
+			),
+			key: fs.readFileSync(
+				path.join(process.cwd(), 'config', 'localhost.key.pem'),
+				'utf8'
+			)
+		}
 	},
 	plugins: [react()],
 	build: {
