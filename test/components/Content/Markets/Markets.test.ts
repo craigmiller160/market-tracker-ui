53b919a7b20b2dc6301977b966b969320dfdfa17
Craig Miller <craigmiller160@gmail.com>
2022-02-28 12:24:40 -0500

    Integrated mock api setup into new tests

--- test/components/Content/Markets/Markets.test.ts
+++ test/components/Content/Markets/Markets.test.ts
@@ -5,6 +5,7 @@ import { pipe } from 'fp-ts/es6/function';
 import * as Try from '@craigmiller160/ts-functions/es/Try';
 import { MarketInvestmentInfo } from '../../../../src/types/data/MarketInvestmentInfo';
 import { createSetupMockApiCalls } from './setupMarketTestData';
+import { MarketTime } from '../../../../src/types/MarketTime';
 
 const mockApi = new MockAdapter(ajaxApi.instance);
 const investmentInfo: ReadonlyArray<MarketInvestmentInfo> = pipe(
@@ -20,34 +21,59 @@ describe('Markets', () => {
 	});
 
 	it('renders for today', async () => {
+		setupMockApiCalls({
+			time: MarketTime.ONE_DAY
+		});
 		throw new Error();
 	});
 
 	it('renders for today when history has higher millis than current time', async () => {
+		setupMockApiCalls({
+			time: MarketTime.ONE_DAY
+		});
 		throw new Error();
 	});
 
 	it('renders for today with market closed', async () => {
+		setupMockApiCalls({
+			time: MarketTime.ONE_DAY,
+			status: 'closed'
+		});
 		throw new Error();
 	});
 
 	it('renders for 1 week', async () => {
+		setupMockApiCalls({
+			time: MarketTime.ONE_WEEK
+		});
 		throw new Error();
 	});
 
 	it('renders for 1 month', async () => {
+		setupMockApiCalls({
+			time: MarketTime.ONE_MONTH
+		});
 		throw new Error();
 	});
 
 	it('renders for 3 months', async () => {
+		setupMockApiCalls({
+			time: MarketTime.THREE_MONTHS
+		});
 		throw new Error();
 	});
 
 	it('renders for 1 year', async () => {
+		setupMockApiCalls({
+			time: MarketTime.ONE_YEAR
+		});
 		throw new Error();
 	});
 
 	it('renders for 5 years', async () => {
+		setupMockApiCalls({
+			time: MarketTime.FIVE_YEARS
+		});
 		throw new Error();
 	});
 });
