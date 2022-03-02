d4edcdcdc5590a7045394e8de723d19c9b726aab
Craig Miller <craigmiller160@gmail.com>
2022-03-02 10:12:51 -0500

    Stubbed out new tests

--- test/services/AjaxApi.Error.test.ts
+++ test/services/AjaxApi.Error.test.ts
@@ -17,55 +17,75 @@ jest.mock('../../src/store', () => {
 const mockApi = new MockAdapter(ajaxApi.instance);
 const mockStore = store as MockStore;
 
-describe('AjaxApi Error Handler', () => {
+describe('AjaxApi Error Handling', () => {
 	beforeEach(() => {
 		mockApi.reset();
 		mockStore.clearActions();
 	});
 
-	it('is 500 error, no body', async () => {
-		mockApi.onGet('/foo').reply(500);
-		await ajaxApi.get({
-			uri: '/foo'
-		})();
-		expect(mockStore.getActions()).toEqual([
-			{
-				type: 'notification/addError',
-				payload:
-					'500 - Message: Request failed with status code 500 Body: {}'
-			}
-		]);
+	describe('isNestedAxiosError', () => {
+		it('is axios error', () => {
+			throw new Error();
+		});
+
+		it('is not axios error', () => {
+			throw new Error();
+		});
+
+		it('is trace error with axios error cause', () => {
+			throw new Error();
+		});
+
+		it('is trace error without axios error cause', () => {
+			throw new Error();
+		});
 	});
 
-	it('is 401 error, no body', async () => {
-		mockApi.onGet('/foo').reply(401);
-		await ajaxApi.get({
-			uri: '/foo'
-		})();
-		await sleep550ms();
-		expect(mockStore.getActions()).toEqual([
-			{
-				type: 'auth/setUserData',
-				payload: Option.none
-			},
-			{
-				type: 'notification/addError',
-				payload: 'Unauthorized'
-			}
-		]);
-	});
+	describe('Error handler', () => {
+		it('is 500 error, no body', async () => {
+			mockApi.onGet('/foo').reply(500);
+			await ajaxApi.get({
+				uri: '/foo'
+			})();
+			expect(mockStore.getActions()).toEqual([
+				{
+					type: 'notification/addError',
+					payload:
+						'500 - Message: Request failed with status code 500 Body: {}'
+				}
+			]);
+		});
 
-	it('is 500 error, with body', async () => {
-		mockApi.onGet('/foo').reply(500, { hello: 'world' });
-		await ajaxApi.get({
-			uri: '/foo'
-		})();
-		const text = JSON.stringify({ hello: 'world' });
-		expect(mockStore.getActions()).toEqual([
-			{
-				type: 'notification/addError',
-				payload: `500 - Message: Request failed with status code 500 Body: ${text}`
-			}
-		]);
+		it('is 401 error, no body', async () => {
+			mockApi.onGet('/foo').reply(401);
+			await ajaxApi.get({
+				uri: '/foo'
+			})();
+			await sleep550ms();
+			expect(mockStore.getActions()).toEqual([
+				{
+					type: 'auth/setUserData',
+					payload: Option.none
+				},
+				{
+					type: 'notification/addError',
+					payload: 'Unauthorized'
+				}
+			]);
+		});
+
+		it('is 500 error, with body', async () => {
+			mockApi.onGet('/foo').reply(500, { hello: 'world' });
+			await ajaxApi.get({
+				uri: '/foo'
+			})();
+			const text = JSON.stringify({ hello: 'world' });
+			expect(mockStore.getActions()).toEqual([
+				{
+					type: 'notification/addError',
+					payload: `500 - Message: Request failed with status code 500 Body: ${text}`
+				}
+			]);
+		});
 	});
 });
