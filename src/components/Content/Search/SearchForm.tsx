05b7e560da04cc116ab213597bf72aa189ba3cf8
Craig Miller <craigmiller160@gmail.com>
2022-04-01 22:31:03 -0400

    Beginning to implement search form

---
+++ src/components/Content/Search/SearchForm.tsx
@@ -0,0 +1,19 @@
+import { Form, Input } from 'antd';
+
+const onFinish = (values: any) => {
+	console.log('OnFinish', values);
+};
+
+const onFinishFailed = (errorInfo: any) => {
+	console.log('OnFinishFailed', errorInfo);
+};
+
+export const SearchForm = () => {
+	return (
+		<Form>
+			<Form.Item label="Search" name="search">
+				<Input />
+			</Form.Item>
+		</Form>
+	);
+};
--- src/components/Content/Search/index.tsx
+++ src/components/Content/Search/index.tsx
@@ -1,8 +1,10 @@
 import './Search.scss';
 import { Typography } from 'antd';
+import { SearchForm } from './SearchForm';
 
 export const Search = () => (
 	<div className="SearchPage">
 		<Typography.Title>Search</Typography.Title>
+		<SearchForm />
 	</div>
 );
