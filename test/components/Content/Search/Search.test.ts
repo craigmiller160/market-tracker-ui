ef877c5b80099c646654243cf4a8f276237840b4
Craig Miller <craigmiller160@gmail.com>
2022-04-02 09:43:21 -0400

    More test improvements

--- test/components/Content/Search/Search.test.ts
+++ test/components/Content/Search/Search.test.ts
@@ -1,11 +1,15 @@
 import MockAdapter from 'axios-mock-adapter';
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import { ajaxApi } from '../../../../src/services/AjaxApi';
 import { createRenderApp } from '../../../testutils/RenderApp';
 import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
 
 const mockApi = new MockAdapter(ajaxApi.instance);
 const renderApp = createRenderApp(mockApi);
+
+const getSearchBtn = () => screen.getByRole('button', { name: 'Search' });
+const getSymbolField = () => screen.getByPlaceholderText('Symbol');
 
 describe('Search', () => {
 	it('renders initial layout correctly', async () => {
@@ -15,11 +19,20 @@ describe('Search', () => {
 		expect(screen.queryByText('Search For Investment')).toBeInTheDocument();
 		expect(screen.queryByText('Stock')).toBeInTheDocument();
 		expect(screen.queryByText('Crypto')).toBeInTheDocument();
-		expect(screen.queryAllByText('Search')).toHaveLength(2);
+		expect(
+			screen.queryByRole('button', { name: 'Search' })
+		).toBeInTheDocument();
+		expect(getSearchBtn()).toBeDisabled();
 		expect(screen.queryByPlaceholderText('Symbol')).toBeInTheDocument();
 	});
 
 	it('formats text and enables/disables search button when symbol input provided', async () => {
-		throw new Error();
+		await renderApp({
+			initialPath: '/market-tracker/search'
+		});
+		expect(getSearchBtn()).toBeDisabled();
+		userEvent.type(getSymbolField(), 'hello');
+		await waitFor(() => expect(getSymbolField()).toHaveValue('HELLO'));
+		expect(getSearchBtn()).toBeEnabled();
 	});
 });
