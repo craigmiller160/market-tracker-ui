fa4e6201d9c78c3c786cb58ba3259d4710cf4517
Craig Miller <craigmiller160@gmail.com>
2022-04-02 16:27:06 -0400

    Refactored and cleaned up test logic

--- test/components/Content/Search/Search.test.ts
+++ test/components/Content/Search/Search.test.ts
@@ -5,70 +5,16 @@ import { createRenderApp } from '../../../testutils/RenderApp';
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
 import { MarketTime } from '../../../../src/types/MarketTime';
-import { formatHistoryDate } from '../../../../src/utils/timeUtils';
 import {
-	getHistoryStart,
-	getTradierInterval
+	mockTradierHistoryRequest,
+	mockTradierQuoteRequest
 } from '../../../testutils/testDataUtils';
-import { TradierCalendarStatus } from '../../../../src/types/tradier/calendar';
-import { createMockCalendar } from '../Markets/setupMarketTestData';
 
 const mockApi = new MockAdapter(ajaxApi.instance);
 const renderApp = createRenderApp(mockApi);
 
 const getSearchBtn = () => screen.getByRole('button', { name: 'Search' });
 const getSymbolField = () => screen.getByPlaceholderText('Symbol');
-
-const mockCalendarRequest = (status?: TradierCalendarStatus) => {
-	const theStatus: TradierCalendarStatus = status ?? 'open';
-	mockApi
-		.onGet(`/tradier/markets/calendar?year=${year}&month=${month}`)
-		.reply(200, createMockCalendar(formattedDate, status));
-}
-
-const mockTradierQuote = (symbol: string) =>
-	mockApi.onGet(`/tradier/markets/quotes?symbols=${symbol}`).reply(200, {
-		quotes: {
-			quote: {
-				symbol,
-				description: 'My Stock',
-				open: 0,
-				high: 0,
-				low: 0,
-				bid: 0,
-				ask: 0,
-				close: 0,
-				last: 100,
-				prevclose: 200
-			},
-			unmatched_symbols: undefined
-		}
-	});
-const mockTradierHistory = (
-	symbol: string,
-	startDate: Date,
-	interval: string
-) => {
-	const start = formatHistoryDate(startDate);
-	const end = formatHistoryDate(new Date());
-	return mockApi
-		.onGet(
-			`/tradier/markets/history?symbol=${symbol}&start=${start}&end=${end}&interval=${interval}`
-		)
-		.reply(200, {
-			history: {
-				day: [
-					{
-						date: '2022-01-01',
-						open: 100,
-						high: 0,
-						low: 0,
-						close: 200
-					}
-				]
-			}
-		});
-};
 
 describe('Search', () => {
 	it('renders initial layout correctly', async () => {
@@ -101,12 +47,8 @@ describe('Search', () => {
 
 	it('searches for and finds a stock for Today', async () => {
 		const time = MarketTime.ONE_DAY;
-		mockTradierQuote('VTI');
-		mockTradierHistory(
-			'VTI',
-			getHistoryStart(time),
-			getTradierInterval(time)
-		);
+		mockTradierQuoteRequest(mockApi, 'VTI', 1);
+		mockTradierHistoryRequest(mockApi, 'VTI', time, 1);
 		await renderApp({
 			initialPath: '/market-tracker/search'
 		});
