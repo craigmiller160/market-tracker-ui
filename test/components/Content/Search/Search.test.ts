cf8d1297aa72f70f33bbdf3f1712cb2774f34b87
Craig Miller <craigmiller160@gmail.com>
2022-04-02 09:19:18 -0400

    Stubbed out tests

---
+++ test/components/Content/Search/Search.test.ts
@@ -0,0 +1,11 @@
+export {};
+
+describe('Search', () => {
+	it('renders initial layout correctly', () => {
+		throw new Error();
+	});
+
+	it('enables/disables search button based on symbol content', () => {
+		throw new Error();
+	});
+});
