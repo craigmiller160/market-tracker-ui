f293770e4e8cc102250a76d669dfa390b020f13c
Craig Miller <craigmiller160@gmail.com>
2022-04-01 22:27:46 -0400

    Building some pieces of logic for the page

--- src/components/Content/Search/index.tsx
+++ src/components/Content/Search/index.tsx
@@ -1,1 +1,8 @@
-export const Search = () => <h1>Search</h1>;
+import './Search.scss';
+import { Typography } from 'antd';
+
+export const Search = () => (
+	<div className="SearchPage">
+		<Typography.Title>Search</Typography.Title>
+	</div>
+);
