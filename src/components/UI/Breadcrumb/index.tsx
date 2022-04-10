374f165238c2637b8e9dd12f1b169215e5743706
Craig Miller <craigmiller160@gmail.com>
2022-04-10 16:23:21 -0400

    Scaffolded breadcrumb

---
+++ src/components/UI/Breadcrumb/index.tsx
@@ -0,0 +1,10 @@
+import { Breadcrumb as AntBreadcrumb } from 'antd';
+
+export const Breadcrumb = () => {
+	return (
+		<AntBreadcrumb>
+			<AntBreadcrumb.Item>Hello</AntBreadcrumb.Item>
+			<AntBreadcrumb.Item>World</AntBreadcrumb.Item>
+		</AntBreadcrumb>
+	);
+};
