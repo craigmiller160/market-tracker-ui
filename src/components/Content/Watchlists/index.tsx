791e4e64d780e24ffc9a72153c4a3647ad2c3ddc
Craig Miller <craigmiller160@gmail.com>
2022-04-14 18:22:07 -0400

    Making progress with panels

--- src/components/Content/Watchlists/index.tsx
+++ src/components/Content/Watchlists/index.tsx
@@ -1,23 +1,35 @@
 import './Watchlists.scss';
-import { Typography } from 'antd';
+import { Typography, Collapse } from 'antd';
 import { useContext } from 'react';
 import { ScreenContext } from '../../ScreenContext';
 import { match } from 'ts-pattern';
+import { Breakpoints, getBreakpointName } from '../../utils/Breakpoints';
 
-const useTitleSpace = () => {
-	const { breakpoints } = useContext(ScreenContext);
-	return match(breakpoints)
+const getTitleSpace = (breakpoints: Breakpoints): string | JSX.Element =>
+	match(breakpoints)
 		.with({ xs: true }, () => <br />)
 		.otherwise(() => ' ');
-};
 
 export const Watchlists = () => {
-	const titleSpace = useTitleSpace();
+	const { breakpoints } = useContext(ScreenContext);
+	const titleSpace = getTitleSpace(breakpoints);
+	const breakpointName = getBreakpointName(breakpoints);
 	return (
-		<div className="WatchlistsPage" data-testid="watchlist-page">
+		<div
+			className={`WatchlistsPage ${breakpointName}`}
+			data-testid="watchlist-page"
+		>
 			<Typography.Title>
 				Investment{titleSpace}Watchlists
 			</Typography.Title>
+			<Collapse className="Accordion" accordion>
+				<Collapse.Panel key="1" header="Panel 1">
+					<p>This is Panel 1</p>
+				</Collapse.Panel>
+				<Collapse.Panel key="2" header="Panel 2">
+					<p>This is Panel 2</p>
+				</Collapse.Panel>
+			</Collapse>
 		</div>
 	);
 };
