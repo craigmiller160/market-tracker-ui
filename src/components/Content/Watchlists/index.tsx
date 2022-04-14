936639aead19d6638a9d29f6d42e3b3a94cca50d
Craig Miller <craigmiller160@gmail.com>
2022-04-14 18:14:39 -0400

    Adjusted title for mobile layout

--- src/components/Content/Watchlists/index.tsx
+++ src/components/Content/Watchlists/index.tsx
@@ -1,7 +1,17 @@
 import './Watchlists.scss';
+import { Typography } from 'antd';
+import { useContext } from 'react';
+import { ScreenContext } from '../../ScreenContext';
+import { match } from 'ts-pattern';
 
-export const Watchlists = () => (
-	<div className="WatchlistsPage">
-		<h1>Investment Watchlists</h1>
-	</div>
-);
+export const Watchlists = () => {
+	const { breakpoints } = useContext(ScreenContext);
+	const space = match(breakpoints)
+		.with({ xs: true }, () => <br />)
+		.otherwise(() => ' ');
+	return (
+		<div className="WatchlistsPage" data-testid="watchlist-page">
+			<Typography.Title>Investment{space}Watchlists</Typography.Title>
+		</div>
+	);
+};
