51bd7a3a8d98d0d7a7de704e85f0164647e24815
Craig Miller <craigmiller160@gmail.com>
2022-04-14 19:35:23 -0400

    Implemented the investment cards for watchlists

--- src/components/Content/Watchlists/WatchlistSection.tsx
+++ src/components/Content/Watchlists/WatchlistSection.tsx
@@ -1,8 +1,32 @@
 import { WatchlistItem } from '../../../types/Watchlist';
+import { InvestmentCard } from '../common/InvestmentCard/InvestmentCard';
+import { InvestmentInfo } from '../../../types/data/InvestmentInfo';
+import { InvestmentType } from '../../../types/data/InvestmentType';
 
 interface Props {
 	readonly stocks: ReadonlyArray<WatchlistItem>;
 	readonly cryptos: ReadonlyArray<WatchlistItem>;
 }
 
-export const WatchlistSection = (props: Props) => <h3>The Watchlist</h3>;
+const symbolToCard =
+	(type: InvestmentType) =>
+	// eslint-disable-next-line react/display-name
+	({ symbol }: WatchlistItem) => {
+		const info: InvestmentInfo = {
+			type,
+			symbol,
+			name: ''
+		};
+		return <InvestmentCard key={symbol} info={info} />;
+	};
+
+export const WatchlistSection = (props: Props) => {
+	const stockCards = props.stocks.map(symbolToCard(InvestmentType.STOCK));
+	const cryptoCards = props.cryptos.map(symbolToCard(InvestmentType.CRYPTO));
+	return (
+		<div className="WatchlistSection" role="list">
+			{stockCards}
+			{cryptoCards}
+		</div>
+	);
+};
