7ef3abb7cac48bcdf4ebc97ab19e896c0f5c305f
Craig Miller <craigmiller160@gmail.com>
2022-04-14 18:56:09 -0400

    Making progress

--- src/components/Content/Watchlists/index.tsx
+++ src/components/Content/Watchlists/index.tsx
@@ -1,19 +1,59 @@
 import './Watchlists.scss';
 import { Typography, Collapse } from 'antd';
-import { useContext } from 'react';
+import { ReactNode, useContext, useEffect } from 'react';
 import { ScreenContext } from '../../ScreenContext';
 import { match } from 'ts-pattern';
 import { Breakpoints, getBreakpointName } from '../../utils/Breakpoints';
+import { Watchlist } from '../../../types/Watchlist';
+import { useImmer } from 'use-immer';
+import { getAllWatchlists } from '../../../services/WatchlistService';
+import { pipe } from 'fp-ts/es6/function';
+import * as TaskEither from 'fp-ts/es6/TaskEither';
+import { castDraft } from 'immer';
+
+interface State {
+	readonly watchlists: ReadonlyArray<Watchlist>;
+}
 
 const getTitleSpace = (breakpoints: Breakpoints): string | JSX.Element =>
 	match(breakpoints)
 		.with({ xs: true }, () => <br />)
 		.otherwise(() => ' ');
 
+const createPanels = (watchlists: ReadonlyArray<Watchlist>): ReactNode =>
+	watchlists.map((watchlist) => (
+		<Collapse.Panel
+			key={watchlist._id}
+			header={
+				<Typography.Title level={4}>
+					{watchlist.watchlistName}
+				</Typography.Title>
+			}
+		>
+			<h3>This is the Watchlist</h3>
+		</Collapse.Panel>
+	));
+
 export const Watchlists = () => {
+	const [state, setState] = useImmer<State>({
+		watchlists: []
+	});
+
+	useEffect(() => {
+		pipe(
+			getAllWatchlists(),
+			TaskEither.map((watchlists) =>
+				setState((draft) => {
+					draft.watchlists = castDraft(watchlists);
+				})
+			)
+		)();
+	}, [setState]);
+
 	const { breakpoints } = useContext(ScreenContext);
 	const titleSpace = getTitleSpace(breakpoints);
 	const breakpointName = getBreakpointName(breakpoints);
+	const panels = createPanels(state.watchlists);
 	return (
 		<div
 			className={`WatchlistsPage ${breakpointName}`}
@@ -23,22 +63,7 @@ export const Watchlists = () => {
 				Investment{titleSpace}Watchlists
 			</Typography.Title>
 			<Collapse className="Accordion" accordion>
-				<Collapse.Panel
-					key="1"
-					header={
-						<Typography.Title level={4}>Panel 1</Typography.Title>
-					}
-				>
-					<p>This is Panel 1</p>
-				</Collapse.Panel>
-				<Collapse.Panel
-					key="2"
-					header={
-						<Typography.Title level={4}>Panel 2</Typography.Title>
-					}
-				>
-					<p>This is Panel 2</p>
-				</Collapse.Panel>
+				{panels}
 			</Collapse>
 		</div>
 	);
