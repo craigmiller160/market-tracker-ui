6042a9280ed1d611f090bb2fe4f4c325e008bb87
Craig Miller <craigmiller160@gmail.com>
2022-04-15 17:52:17 -0400

    Continuing to play with mirage-js db

--- test/testutils/server/index.ts
+++ test/testutils/server/index.ts
@@ -1,11 +1,23 @@
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { Server } from 'miragejs/server';
 import { NanoidIdentityManager } from './NanoidIdentityManager';
+import { ModelDefinition, Registry } from 'miragejs/-types';
+import Schema from 'miragejs/orm/schema';
+
+export interface Movie extends ModelDefinition<Movie> {
+	readonly title: string;
+}
+
+type AppRegistry = Registry<{ movies: Movie }, Record<string, any>>;
+type AppSchema = Schema<AppRegistry>;
 
 export const newTestServer = (): Server =>
 	createServer({
 		identityManagers: new NanoidIdentityManager(),
-		seeds(server) {
+		models: {
+			movies: Model
+		},
+		seeds(server: Server<AppRegistry>) {
 			server.db.loadData({
 				movies: [
 					{ title: 'LOTR' },
@@ -18,8 +30,8 @@ export const newTestServer = (): Server =>
 			this.namespace = '/market-tracker/api';
 
 			// TODO how to properly type schema.db?
-			this.get('/movies', (schema) => schema.db.movies);
-			this.post('/movies', (schema, request) => {
+			this.get('/movies', (schema: AppSchema) => schema.db.movies);
+			this.post('/movies', (schema: AppSchema, request) => {
 				const movie = JSON.parse(request.requestBody);
 				return schema.db.movies.insert(movie);
 			});
