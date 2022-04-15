fcb071d8d0a3df52d4944f686b8e58d627ac9e2b
Craig Miller <craigmiller160@gmail.com>
2022-04-14 20:28:05 -0400

    Continuing to prep refactor

--- src/components/Content/Watchlists/createWatchlistPanel.tsx
+++ src/components/Content/Watchlists/createWatchlistPanel.tsx
@@ -1,19 +1,28 @@
 import { Watchlist } from '../../../types/Watchlist';
 import { Collapse, Typography } from 'antd';
 import { WatchlistSection } from './WatchlistSection';
+import './WatchlistPanel.scss';
 
-export const createWatchlistPanel = (watchlist: Watchlist) => (
-	<Collapse.Panel
-		key={watchlist._id}
-		header={
-			<Typography.Title level={4}>
-				{watchlist.watchlistName}
-			</Typography.Title>
-		}
-	>
-		<WatchlistSection
-			stocks={watchlist.stocks}
-			cryptos={watchlist.cryptos}
-		/>
-	</Collapse.Panel>
-);
+const createTitle = (watchlist: Watchlist) => {
+	return (
+		<Typography.Title className="PanelTitle" level={4}>
+			{watchlist.watchlistName}
+		</Typography.Title>
+	);
+};
+
+export const createWatchlistPanel = (watchlist: Watchlist) => {
+	const title = createTitle(watchlist);
+	return (
+		<Collapse.Panel
+			key={watchlist._id}
+			className="WatchlistPanel"
+			header={title}
+		>
+			<WatchlistSection
+				stocks={watchlist.stocks}
+				cryptos={watchlist.cryptos}
+			/>
+		</Collapse.Panel>
+	);
+};
