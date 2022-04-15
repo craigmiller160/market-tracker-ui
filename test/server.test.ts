2afd7bf1ff98a66df703ca869bdd52695d6dbc5a
Craig Miller <craigmiller160@gmail.com>
2022-04-15 17:15:25 -0400

    Stubbed another test

--- test/server.test.ts
+++ test/server.test.ts
@@ -1,7 +1,13 @@
 export {};
 
+// TODO delete this in the end
+
 describe('mirage server', () => {
-	it('makes request', () => {
+	it('gets data with request', () => {
+		throw new Error();
+	});
+
+	it('modifies mirage data with request', () => {
 		throw new Error();
 	});
 });
