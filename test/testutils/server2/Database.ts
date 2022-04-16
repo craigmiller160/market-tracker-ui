baefe75ad37d85f5baa1134d3800a0fc131d238a
Craig Miller <craigmiller160@gmail.com>
2022-04-16 11:42:47 -0400

    Beginning to fully implement new API server

--- test/testutils/server2/Database.ts
+++ test/testutils/server2/Database.ts
@@ -1,12 +1,15 @@
 import { nanoid } from '@reduxjs/toolkit';
 import { WritableDraft } from 'immer/dist/types/types-external';
 import produce from 'immer';
+import { DbWatchlist } from '../../../src/types/Watchlist';
 
 export interface DbRecord {
 	readonly _id: string;
 }
 
-export interface Data {}
+export interface Data {
+	readonly watchlists: ReadonlyArray<DbWatchlist>;
+}
 
 export const ensureDbRecord = <T extends object>(record: T): T & DbRecord => {
 	if (
@@ -23,12 +26,10 @@ export const ensureDbRecord = <T extends object>(record: T): T & DbRecord => {
 
 export class Database {
 	data: Data = {
-		movies: []
+		watchlists: []
 	};
 
 	updateData(updater: (draft: WritableDraft<Data>) => void) {
 		this.data = produce(this.data, updater);
 	}
 }
-
-export const database = new Database();
--- test/testutils/server2/index.ts
+++ test/testutils/server2/index.ts
@@ -1,9 +1,14 @@
 import { createServer } from 'miragejs';
 import { Server } from 'miragejs/server';
+import { Database } from './Database';
 
-export const newApiServer = (): Server =>
-	createServer({
+export const newApiServer = (): Server => {
+	const database = new Database();
+	return createServer({
 		routes() {
 			this.namespace = '/market-tracker/api';
+
+			this.get('/watchlists/all', () => database.data.watchlists);
 		}
 	});
+};
