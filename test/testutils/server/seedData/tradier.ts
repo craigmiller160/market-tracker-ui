5675cde77087ac86a513ca0d35d2ed71487697f0
Craig Miller <craigmiller160@gmail.com>
2022-04-16 14:19:33 -0400

    Working on tradier seed data

--- test/testutils/server/seedData/tradier.ts
+++ test/testutils/server/seedData/tradier.ts
@@ -1,5 +1,19 @@
-import { DataUpdater } from '../Database';
+import { Data, DataUpdater } from '../Database';
+import { createTradierQuote } from '../../testDataUtils';
+import { castDraft } from 'immer';
+import { WritableDraft } from 'immer/dist/types/types-external';
+
+const createAddQuote =
+	(draft: WritableDraft<Data>) => (symbol: string, modifier: number) => {
+		draft.tradier.quotes[symbol] = castDraft(
+			createTradierQuote(symbol, modifier)
+		);
+	};
 
 export const seedTradier: DataUpdater = (draft) => {
-
+	const addQuote = createAddQuote(draft);
+	addQuote('VTI', 1);
+	addQuote('VOO', 2);
+	addQuote('AAPL', 3);
+	addQuote('GOOG', 4);
 };
