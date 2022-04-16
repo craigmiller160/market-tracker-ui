cce128bad42ccdb88e1cd3eb59d28749b18f5471
Craig Miller <craigmiller160@gmail.com>
2022-04-16 09:13:12 -0400

    Fixing lint issues

--- test/testutils/server/index.ts
+++ test/testutils/server/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment, @typescript-eslint/no-explicit-any */
 import { createServer, Model } from 'miragejs';
 import { Server } from 'miragejs/server';
 import { NanoidIdentityManager } from './NanoidIdentityManager';
