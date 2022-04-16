f63c2a8b802c8c7b098d60d2bd484f68a482f507
Craig Miller <craigmiller160@gmail.com>
2022-04-16 11:51:09 -0400

    Improved return type of ApiServer

--- test/testutils/server2/index.ts
+++ test/testutils/server2/index.ts
@@ -2,13 +2,24 @@ import { createServer } from 'miragejs';
 import { Server } from 'miragejs/server';
 import { Database } from './Database';
 
-export const newApiServer = (): Server => {
+interface ApiServer {
+	readonly server: Server;
+	readonly shutdown: () => void;
+	readonly database: Database;
+}
+
+export const newApiServer = (): ApiServer => {
 	const database = new Database();
-	return createServer({
+	const server: Server = createServer({
 		routes() {
 			this.namespace = '/market-tracker/api';
 
 			this.get('/watchlists/all', () => database.data.watchlists);
 		}
 	});
+	return {
+		server,
+		database,
+		shutdown: server.shutdown
+	};
 };
