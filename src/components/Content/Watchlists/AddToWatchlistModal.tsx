56da91a7e542aaccbd8f700928beace98a00e20b
Craig Miller <craigmiller160@gmail.com>
2022-04-16 17:19:29 -0400

    Refined the appearance of radio buttons

--- src/components/Content/Watchlists/AddToWatchlistModal.tsx
+++ src/components/Content/Watchlists/AddToWatchlistModal.tsx
@@ -1,13 +1,20 @@
-import { Modal } from 'antd';
+import { Form, Modal, Radio, Space } from 'antd';
 
 interface Props {
 	readonly show: boolean;
 	readonly onClose: () => void;
 }
 
+type WatchlistSelectionType = 'existing' | 'new';
+
+interface ModalForm {
+	readonly watchlistSelectionType: WatchlistSelectionType;
+}
+
 // TODO don't forget alert message on success
 
 export const AddToWatchlistModal = (props: Props) => {
+	const [form] = Form.useForm<ModalForm>();
 	return (
 		<Modal
 			title="Add to Watchlist"
@@ -15,8 +22,19 @@ export const AddToWatchlistModal = (props: Props) => {
 			onCancel={props.onClose}
 		>
 			<div>
-				<p>Radio & Select Box</p>
-				<p>Radio & Input Field</p>
+				<Form
+					form={form}
+					initialValues={{
+						watchlistSelectionType: 'existing'
+					}}
+				>
+					<Radio.Group defaultValue="existing">
+						<Space direction="vertical">
+							<Radio value="existing">Select Box</Radio>
+							<Radio value="new">Input Field</Radio>
+						</Space>
+					</Radio.Group>
+				</Form>
 			</div>
 		</Modal>
 	);
