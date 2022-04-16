0827cb79f69df4883175a4b3ae056806de00c1bd
Craig Miller <craigmiller160@gmail.com>
2022-04-16 15:09:02 -0400

    Implemented even more test logic

--- test/components/Content/Watchlists/Watchlists.test.ts
+++ test/components/Content/Watchlists/Watchlists.test.ts
@@ -1,9 +1,11 @@
 import { ApiServer, newApiServer } from '../../../testutils/server';
 import { renderApp } from '../../../testutils/RenderApp';
-import { waitFor } from '@testing-library/react';
+import { waitFor, within } from '@testing-library/react';
 import { screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
+
+const PRICE_REGEX = /\$\d+\.\d{2}/;
 
 describe('Watchlists', () => {
 	let apiServer: ApiServer;
@@ -39,6 +41,22 @@ describe('Watchlists', () => {
 		await waitFor(() =>
 			expect(screen.queryAllByText('Chart is Here')).toHaveLength(2)
 		);
+
+		const vtiCard = screen.getByTestId('market-card-VTI');
+		expect(within(vtiCard).queryByText(/VTI/)).toHaveTextContent(
+			'My Stock (VTI)'
+		);
+		const vtiPriceNodes = within(vtiCard).getAllByText(PRICE_REGEX);
+		expect(vtiPriceNodes).toHaveLength(2);
+		expect(vtiPriceNodes[0]).toHaveTextContent('$100.00');
+
+		const vooCard = screen.getByTestId('market-card-VOO');
+		expect(within(vooCard).queryByText(/VOO/)).toHaveTextContent(
+			'My Stock (VOO)'
+		);
+		const vooPriceNodes = within(vooCard).getAllByText(PRICE_REGEX);
+		expect(vooPriceNodes).toHaveLength(2);
+		expect(vooPriceNodes[0]).toHaveTextContent('$101.00');
 	});
 
 	it('renames a watchlist', async () => {
