18069bd6940bca316717ebb5243d862a4d2dbf6f
Craig Miller <craigmiller160@gmail.com>
2022-04-17 20:22:41 -0400

    Progress setting up remove investment button

--- src/components/Content/Watchlists/WatchlistSection.tsx
+++ src/components/Content/Watchlists/WatchlistSection.tsx
@@ -3,6 +3,8 @@ import { InvestmentCard } from '../common/InvestmentCard/InvestmentCard';
 import { InvestmentInfo } from '../../../types/data/InvestmentInfo';
 import { InvestmentType } from '../../../types/data/InvestmentType';
 import './WatchlistSection.scss';
+import { ReactNode } from 'react';
+import { Button } from 'antd';
 
 interface Props {
 	readonly watchlistName: string;
@@ -10,8 +12,18 @@ interface Props {
 	readonly cryptos: ReadonlyArray<WatchlistItem>;
 }
 
+const createGetActions =
+	(watchlistName: string, type: InvestmentType) =>
+	// eslint-disable-next-line react/display-name
+	(symbol: string): ReactNode[] => {
+		if (InvestmentType.CRYPTO === type) {
+			return [];
+		}
+		return [<Button key="remove">Remove</Button>];
+	};
+
 const symbolToCard =
-	(type: InvestmentType) =>
+	(watchlistName: string, type: InvestmentType) =>
 	// eslint-disable-next-line react/display-name
 	({ symbol }: WatchlistItem) => {
 		const info: InvestmentInfo = {
@@ -19,12 +31,19 @@ const symbolToCard =
 			symbol,
 			name: ''
 		};
-		return <InvestmentCard key={symbol} info={info} />;
+		const getActions = createGetActions(watchlistName, type);
+		return (
+			<InvestmentCard key={symbol} info={info} getActions={getActions} />
+		);
 	};
 
 export const WatchlistSection = (props: Props) => {
-	const stockCards = props.stocks.map(symbolToCard(InvestmentType.STOCK));
-	const cryptoCards = props.cryptos.map(symbolToCard(InvestmentType.CRYPTO));
+	const stockCards = props.stocks.map(
+		symbolToCard(props.watchlistName, InvestmentType.STOCK)
+	);
+	const cryptoCards = props.cryptos.map(
+		symbolToCard(props.watchlistName, InvestmentType.CRYPTO)
+	);
 	return (
 		<div className="WatchlistSection" role="list">
 			{stockCards}
