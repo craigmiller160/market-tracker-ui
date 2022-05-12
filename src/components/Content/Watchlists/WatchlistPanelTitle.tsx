d7040392bcb393966085ab102ab8553461129a77
Craig Miller <craigmiller160@gmail.com>
2022-05-12 17:12:26 -0400

    Implemented new title component

--- src/components/Content/Watchlists/WatchlistPanelTitle.tsx
+++ src/components/Content/Watchlists/WatchlistPanelTitle.tsx
@@ -1,11 +1,86 @@
-import { Breakpoints } from '../../utils/Breakpoints';
+import { Breakpoints, getBreakpointName } from '../../utils/Breakpoints';
 import { DbWatchlist } from '../../../types/Watchlist';
+import { Button, Form, FormInstance, Input, Typography } from 'antd';
+import { MouseEvent } from 'react';
+import { useDispatch } from 'react-redux';
+import { notificationSlice } from '../../../store/notification/slice';
 
 interface Props {
-    readonly breakpoints: Breakpoints;
-    readonly watchlist: DbWatchlist;
+	readonly breakpoints: Breakpoints;
+	readonly watchlist: DbWatchlist;
+	readonly renameWatchlistId?: string;
+	readonly onSaveRenamedWatchlist: (id: string, newName: string) => void;
+	readonly onClearRenamedWatchlistId: () => void;
 }
 
-export const WatchlistPanelTitle = (props: Props) => {
+interface TitleForm {
+	readonly watchlistName: string;
+}
 
+const createOnSaveRenamedTitle =
+	(
+		form: FormInstance<TitleForm>,
+		onSaveRenamedWatchlist: (newName: string) => void
+	) =>
+	(event: MouseEvent) => {
+		event.stopPropagation();
+		onSaveRenamedWatchlist(form.getFieldsValue().watchlistName);
+	};
+const createOnCancelRenamedTitle =
+	(onClearRenamedWatchlistId: () => void) => (event: MouseEvent) => {
+		event.stopPropagation();
+		onClearRenamedWatchlistId();
+	};
+
+export const WatchlistPanelTitle = (props: Props) => {
+	const { watchlist, renameWatchlistId } = props;
+	const dispatch = useDispatch();
+	const [form] = Form.useForm<TitleForm>();
+
+	const isEditing = watchlist._id === renameWatchlistId;
+
+	if (!isEditing) {
+		return (
+			<Typography.Title level={4} data-testid="watchlist-panel-title">
+				{watchlist.watchlistName}
+			</Typography.Title>
+		);
+	}
+
+	const onSaveRenamedTitle = createOnSaveRenamedTitle(form, (newName) => {
+		props.onSaveRenamedWatchlist(watchlist._id, newName);
+		dispatch(
+			notificationSlice.actions.addSuccess(
+				'Successfully renamed watchlist'
+			)
+		);
+	});
+	const onCancelRenamedTitle = createOnCancelRenamedTitle(
+		props.onClearRenamedWatchlistId
+	);
+	const breakpointName = getBreakpointName(props.breakpoints);
+
+	return (
+		<Form
+			className={`PanelTitleForm ${breakpointName}`}
+			form={form}
+			initialValues={{
+				watchlistName: watchlist.watchlistName
+			}}
+		>
+			<Form.Item name="watchlistName">
+				<Input allowClear onClick={(e) => e.stopPropagation()} />
+			</Form.Item>
+			<div className={`TitleFormActions ${breakpointName}`}>
+				<Button onClick={onCancelRenamedTitle}>Cancel</Button>
+				<Button
+					htmlType="submit"
+					type="primary"
+					onClick={onSaveRenamedTitle}
+				>
+					Save
+				</Button>
+			</div>
+		</Form>
+	);
 };
