723b8c40f2b0cc5944fd5c0c6b64994d3fc3c3e6
Craig Miller <craigmiller160@gmail.com>
2022-05-12 15:32:28 -0400

    More progress building re-usable panel

--- src/components/UI/Accordion/AccordionPanel.tsx
+++ src/components/UI/Accordion/AccordionPanel.tsx
@@ -1,13 +1,22 @@
 import { Collapse } from 'antd';
 import './AccordionPanel.scss';
+import { ReactNode } from 'react';
 
-interface Props {}
+interface Props {
+	readonly title: ReactNode;
+	readonly key: string;
+	readonly actions?: ReactNode;
+}
 
 export const AccordionPanel = (props: Props) => {
-    const { ...rest } = props;
-    return (
-        <Collapse.Panel className="AccordionPanel" {...rest}>
-
-        </Collapse.Panel>
-    );
+	const { title, actions, key, ...rest } = props;
+	return (
+		<Collapse.Panel
+			header={title}
+			key={key}
+			extra={actions}
+			className="AccordionPanel"
+			{...rest}
+		></Collapse.Panel>
+	);
 };
