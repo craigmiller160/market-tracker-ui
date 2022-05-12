61c321c711f0269cc32b58cfa9cfab06946fce66
Craig Miller <craigmiller160@gmail.com>
2022-05-12 15:22:43 -0400

    Began working on Accordion

---
+++ src/components/UI/Accordion/index.tsx
@@ -0,0 +1,13 @@
+import { Collapse } from 'antd';
+
+interface Props {
+
+}
+
+export const Accordion = (props: Props) => {
+	return (
+		<Collapse className="Accordion" accordion destroyInactivePanel>
+			{/*{panels}*/}
+		</Collapse>
+	);
+};
