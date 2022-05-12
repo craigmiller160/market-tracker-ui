e56b3ebfe0552bb69cd30466356583bb04596274
Craig Miller <craigmiller160@gmail.com>
2022-05-12 15:47:47 -0400

    Beginning to rebuild watchlists page

--- src/components/Content/Watchlists/index.tsx
+++ src/components/Content/Watchlists/index.tsx
@@ -1,1 +1,34 @@
-export const Watchlists = () => <h1>Hello World</h1>;
+import { useImmer } from 'use-immer';
+import { DbWatchlist } from '../../../types/Watchlist';
+
+interface State {
+	readonly loading: boolean;
+	readonly watchlists: ReadonlyArray<DbWatchlist>;
+	readonly renameWatchlistId?: string;
+	readonly confirmModal: {
+		readonly show: boolean;
+		readonly message: string;
+		readonly watchlistName: string;
+		readonly symbol?: string;
+	};
+	readonly inputModal: {
+		readonly show: boolean;
+	};
+}
+
+export const Watchlists = () => {
+	const [state, setState] = useImmer<State>({
+		loading: true,
+		watchlists: [],
+		confirmModal: {
+			show: false,
+			message: '',
+			watchlistName: ''
+		},
+		inputModal: {
+			show: false
+		}
+	});
+
+	return <h1>Hello World</h1>;
+};
