e635fe8a4797941e7e96307e5c4250d71854d0f8
Craig Miller <craigmiller160@gmail.com>
2022-05-14 10:41:50 -0400

    Integrated navbar items into new navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,7 @@
 import './Navbar.scss';
 import { Layout, Menu } from 'antd';
 import { useImmer } from 'use-immer';
+import { useNavbarItems } from './useNavbarItems';
 
 interface State {
 	readonly selectedPageKey: string;
@@ -12,11 +13,15 @@ const initState: State = {
 
 export const Navbar = () => {
 	const [state, setState] = useImmer<State>(initState);
+	const Items = useNavbarItems();
 	return (
 		<Layout.Header className="Navbar">
 			<Menu className="Menu" theme="dark" mode="horizontal">
-				<Menu.Item key="Nothing" className="Brand">Market Tracker</Menu.Item>
+				<Menu.Item key="Nothing" className="Brand">
+					Market Tracker
+				</Menu.Item>
+				{Items}
 			</Menu>
 		</Layout.Header>
-	)
+	);
 };
