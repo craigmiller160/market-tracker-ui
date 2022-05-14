bbcffa87ecc13282778543a5c3b76ba628326ec5
Craig Miller <craigmiller160@gmail.com>
2022-05-14 15:54:42 -0400

    Implemented click handling logic and fixed some more mobile styling issues

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,19 @@
 import './Navbar.scss';
 import { Layout, Menu } from 'antd';
-import { useImmer } from 'use-immer';
+import { Updater, useImmer } from 'use-immer';
 import { useNavbarItems } from './useNavbarItems';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { timeMenuKeySelector } from '../../store/marketSettings/selectors';
 import { useBreakpointName } from '../utils/Breakpoints';
+import { NavigateFunction, useLocation, useNavigate } from 'react-router';
+import { useCallback, useEffect } from 'react';
+import { StoreDispatch } from '../../store';
+import { pipe } from 'fp-ts/es6/function';
+import * as Option from 'fp-ts/es6/Option';
+import { match, P } from 'ts-pattern';
+import { changeSelectedTime } from '../../store/marketSettings/actions';
+import * as Regex from '@craigmiller160/ts-functions/es/Regex';
+import { MenuInfo } from 'rc-menu/lib/interface';
 
 interface State {
 	readonly selectedPageKey: string;
@@ -14,14 +23,87 @@ const initState: State = {
 	selectedPageKey: 'page.markets'
 };
 
+interface PathRegexGroups {
+	readonly key: string;
+}
+
+interface MenuKeyParts {
+	readonly prefix: string;
+	readonly action: string;
+}
+
+const PATH_REGEX = /^\/market-tracker\/(?<key>.*)\/?.*$/;
+const MENU_KEY_PARTS_REGEX = /^(?<prefix>.*)\.(?<action>.*)$/;
+const capturePathKey = Regex.capture<PathRegexGroups>(PATH_REGEX);
+const captureMenuKeyParts = Regex.capture<MenuKeyParts>(MENU_KEY_PARTS_REGEX);
+
+const useHandleMenuClick = (
+	navigate: NavigateFunction,
+	dispatch: StoreDispatch
+) =>
+	useCallback(
+		(menuItemInfo: MenuInfo) => {
+			const keyParts = pipe(
+				captureMenuKeyParts(menuItemInfo.key),
+				Option.getOrElse(() => ({
+					prefix: '',
+					action: ''
+				}))
+			);
+
+			match(keyParts)
+				.with({ prefix: 'auth' }, () => {
+					// Do nothing for now
+				})
+				.with({ prefix: 'page', action: P.select() }, (page) => {
+					navigate(`/market-tracker/${page}`);
+				})
+				.with({ prefix: 'time' }, () => {
+					dispatch(changeSelectedTime(menuItemInfo.key));
+				})
+				.otherwise(() => {
+					console.error('Invalid MenuInfo keyParts', keyParts);
+					navigate('/market-tracker/');
+				});
+		},
+		[navigate, dispatch]
+	);
+
+const useSetSelectedFromLocation = (setState: Updater<State>) =>
+	useCallback(
+		(pathname: string) =>
+			pipe(
+				capturePathKey(pathname),
+				Option.map((groups) => {
+					setState((draft) => {
+						draft.selectedPageKey = `page.${groups.key}`;
+					});
+					return groups;
+				})
+			),
+		[setState]
+	);
+
 export const Navbar = () => {
+	const location = useLocation();
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
 	const [state, setState] = useImmer<State>(initState);
 	const selectedTimeKey = useSelector(timeMenuKeySelector);
 	const Items = useNavbarItems(state.selectedPageKey, selectedTimeKey);
 	const breakpointName = useBreakpointName();
+
+	const handleMenuClick = useHandleMenuClick(navigate, dispatch);
+	const setSelectedFromLocation = useSetSelectedFromLocation(setState);
+
+	useEffect(() => {
+		setSelectedFromLocation(location.pathname);
+	}, [location.pathname, setSelectedFromLocation]);
+
 	return (
 		<Layout.Header className={`Navbar ${breakpointName}`}>
 			<Menu
+				onClick={handleMenuClick}
 				className="Menu"
 				theme="dark"
 				mode="horizontal"
