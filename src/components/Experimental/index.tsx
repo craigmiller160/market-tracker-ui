bb078ac092319203837df27ff040422abf065312
Craig Miller <craigmiller160@gmail.com>
2022-05-14 08:42:57 -0400

    Playing with experimental navbar design

---
+++ src/components/Experimental/index.tsx
@@ -0,0 +1,12 @@
+import { Layout } from 'antd';
+import './Experimental.scss';
+
+export const ExperimentalNavbar = () => {
+	return (
+		<Layout.Header className="Experimental">
+			<div className="Brand">
+				<h3>Market Tracker</h3>
+			</div>
+		</Layout.Header>
+	);
+};
--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,11 +1,11 @@
-import { Navbar } from './Navbar';
 import { Content } from './Content';
 import { Layout } from 'antd';
 import './RootLayout.scss';
+import { ExperimentalNavbar } from './Experimental';
 
 export const RootLayout = () => (
 	<Layout className="RootLayout">
-		<Navbar />
+		<ExperimentalNavbar />
 		<Content />
 	</Layout>
 );
