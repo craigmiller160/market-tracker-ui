04c02250aba8a3ee9606b5c2fb54fc98ed396dd6
Craig Miller <craigmiller160@gmail.com>
2023-02-18 14:03:48 -0500

    Fixed compile error

--- src/components/keycloak/MarketTrackerKeycloakProvider.tsx
+++ src/components/keycloak/MarketTrackerKeycloakProvider.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from 'react';
 import { KeycloakAuthProvider } from '@craigmiller160/react-keycloak';
 import { MarketTrackerKeycloakBridge } from './MarketTrackerKeycloakBridge';
+import { BEARER_TOKEN_KEY } from '@craigmiller160/ajax-api-fp-ts';
 
 export const MarketTrackerKeycloakProvider = (props: PropsWithChildren) => (
 	<KeycloakAuthProvider
