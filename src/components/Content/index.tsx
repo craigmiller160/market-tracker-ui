04c978f6d51717baa5bcbdf42ce2f9912023fc44
Craig Miller <craigmiller160@gmail.com>
2023-02-18 14:56:29 -0500

    Fixed early error call

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,19 +1,11 @@
-import { useEffect } from 'react';
-import { loadAuthUser } from '../../store/auth/actions';
 import { Layout } from 'antd';
 import { AppRoutes } from './AppRoutes';
 import { useNotification } from '../UI/Notification/useNotification';
 import { useCheckMarketStatus } from './useCheckMarketStatus';
-import { useStoreDispatch } from '../../store';
 
 export const Content = () => {
-	const dispatch = useStoreDispatch();
 	useNotification();
 	useCheckMarketStatus();
-
-	useEffect(() => {
-		dispatch(loadAuthUser());
-	}, [dispatch]);
 
 	return (
 		<Layout.Content className="MainContent">
