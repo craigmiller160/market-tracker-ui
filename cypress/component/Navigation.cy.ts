6946240dda8e565cfc4fbb8e72b65e075f1ac5f7
Craig Miller <craigmiller160@gmail.com>
2023-02-18 17:22:46 -0500

    Implemented core test logic

--- cypress/component/Navigation.cy.ts
+++ cypress/component/Navigation.cy.ts
@@ -1,7 +1,28 @@
 export {};
 describe('Navigation', () => {
 	it('navigation on mobile does not use selected class', () => {
-		cy.mount();
-		cy.get('.ant-menu-title-content').should('have.text', 'Market Tracker');
+		cy.mount({
+			viewport: 'mobile'
+		});
+		cy.get('.ant-notification-close-x').click();
+		cy.get('.Brand .ant-menu-title-content').should(
+			'have.text',
+			'Market Tracker'
+		);
+		cy.get('.ant-menu-submenu-horizontal .ant-menu-title-content')
+			.should('have.length', 2)
+			.each(($elem, index) => {
+				expect($elem.text()).eq(['Markets', 'Today'][index]);
+			});
+		cy.get('.ant-menu-submenu-horizontal .ant-menu-title-content')
+			.eq(0)
+			.click();
+		cy.get('.ant-menu-sub .ant-menu-title-content').eq(2).click();
+		cy.get('.ant-notification-close-x').click();
+		cy.get('.ant-menu-submenu-horizontal .ant-menu-title-content')
+			.should('have.length', 2)
+			.each(($elem, index) => {
+				expect($elem.text()).eq(['Watchlists', 'Today'][index]);
+			});
 	});
 });
