c3b4a89d67092ba8fd073d3b295dd6cf5a8239d5
Craig Miller <craigmiller160@gmail.com>
2023-02-18 17:04:12 -0500

    Implemetned more test logic

--- cypress/component/Navigation.cy.ts
+++ cypress/component/Navigation.cy.ts
@@ -2,5 +2,6 @@ export {};
 describe('Navigation', () => {
 	it('navigation on mobile does not use selected class', () => {
 		cy.mount();
+		cy.get('.ant-menu-title-content').should('have.text', 'Market Tracker');
 	});
 });
