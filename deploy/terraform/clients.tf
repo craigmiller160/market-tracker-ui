36a25fa2ce20c29a180a60a1724694d0fba4507a
Craig Miller <craigmiller160@gmail.com>
2023-02-18 14:07:42 -0500

    Setup clients

--- deploy/terraform/clients.tf
+++ deploy/terraform/clients.tf
@@ -8,15 +8,15 @@ data "keycloak_realm" "apps_prod" {
 
 locals {
   client_common = {
-    client_id = "expense-tracker-ui"
-    name = "expense-tracker-ui"
+    client_id = "market-tracker-ui"
+    name = "market-tracker-ui"
     enabled = true
     access_type = "PUBLIC"
     standard_flow_enabled = true
   }
 }
 
-resource "keycloak_openid_client" "expense_tracker_ui_dev" {
+resource "keycloak_openid_client" "market_tracker_ui_dev" {
   realm_id = data.keycloak_realm.apps_dev.id
   client_id = local.client_common.client_id
   name = local.client_common.name
@@ -24,17 +24,17 @@ resource "keycloak_openid_client" "expense_tracker_ui_dev" {
   access_type = local.client_common.access_type
   standard_flow_enabled = local.client_common.standard_flow_enabled
   valid_redirect_uris = [
-    "https://localhost:3002/expense-tracker/*"
+    "https://localhost:3000/market-tracker/*"
   ]
   valid_post_logout_redirect_uris = [
-    "https://localhost:3002/expense-tracker/*"
+    "https://localhost:3000/market-tracker/*"
   ]
   web_origins = [
-    "https://localhost:3002"
+    "https://localhost:3000"
   ]
 }
 
-resource "keycloak_openid_client" "expense_tracker_ui_prod" {
+resource "keycloak_openid_client" "market_tracker_ui_prod" {
   realm_id = data.keycloak_realm.apps_prod.id
   client_id = local.client_common.client_id
   name = local.client_common.name
@@ -42,10 +42,10 @@ resource "keycloak_openid_client" "expense_tracker_ui_prod" {
   access_type = local.client_common.access_type
   standard_flow_enabled = local.client_common.standard_flow_enabled
   valid_redirect_uris = [
-    "https://apps-craigmiller160.ddns.net/expense-tracker/*"
+    "https://apps-craigmiller160.ddns.net/market-tracker/*"
   ]
   valid_post_logout_redirect_uris = [
-    "https://apps-craigmiller160.ddns.net/expense-tracker/*"
+    "https://apps-craigmiller160.ddns.net/market-tracker/*"
   ]
   web_origins = [
     "https://apps-craigmiller160.ddns.net"
