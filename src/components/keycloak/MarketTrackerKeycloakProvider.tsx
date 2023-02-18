8335565495bdf25a732ae5d480005bc8adbb3768
Craig Miller <craigmiller160@gmail.com>
2023-02-18 14:11:46 -0500

    Integrated realm setting

--- src/components/keycloak/MarketTrackerKeycloakProvider.tsx
+++ src/components/keycloak/MarketTrackerKeycloakProvider.tsx
@@ -1,11 +1,20 @@
+/// <reference types="vite/client" />
+
 import { PropsWithChildren } from 'react';
 import { KeycloakAuthProvider } from '@craigmiller160/react-keycloak';
 import { MarketTrackerKeycloakBridge } from './MarketTrackerKeycloakBridge';
 import { BEARER_TOKEN_KEY } from '@craigmiller160/ajax-api-fp-ts';
 
+const getRealm = (): string => {
+	if (process.env.NODE_ENV !== 'test') {
+		return import.meta.env.VITE_KEYCLOAK_REALM;
+	}
+	return '';
+};
+
 export const MarketTrackerKeycloakProvider = (props: PropsWithChildren) => (
 	<KeycloakAuthProvider
-		realm=""
+		realm={getRealm()}
 		clientId="market-tracker-ui"
 		localStorageKey={BEARER_TOKEN_KEY}
 	>
