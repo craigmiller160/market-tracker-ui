bf636d5e2d3b3a9259b2f8e449a193f950e92e6b
Craig Miller <craigmiller160@gmail.com>
2023-02-19 10:49:05 -0500

    Stubbed out tests

---
+++ cypress/component/Watchlists.cy.ts
@@ -0,0 +1,10 @@
+export {};
+
+describe('Watchlists', () => {
+	it('the tests to make', () => {
+		// Shows the watchlists in alphabetical order, with the crypos
+		// Remove/Rename on all watchlists except crypto
+		// Remove investment from watchlist, except crypto
+		throw new Error();
+	});
+});
