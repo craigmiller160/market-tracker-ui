456c4a3f8f819e1d627a82e1d60c750d645fb58f
Craig Miller <craigmiller160@gmail.com>
2023-02-18 19:23:05 -0500

    Fixed infinite error spam

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,10 +1,8 @@
 import { Layout } from 'antd';
 import { AppRoutes } from './AppRoutes';
-import { useNotification } from '../UI/Notification/useNotification';
 import { useCheckMarketStatus } from './useCheckMarketStatus';
 
 export const Content = () => {
-	useNotification();
 	useCheckMarketStatus();
 
 	return (
--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -2,10 +2,12 @@ import { Content } from './Content';
 import { Layout } from 'antd';
 import './RootLayout.scss';
 import { Navbar } from './Navbar';
+import { NotificationContainer } from './NotificationContainer';
 
 export const RootLayout = () => (
 	<Layout className="RootLayout">
 		<Navbar />
 		<Content />
+		<NotificationContainer />
 	</Layout>
 );
