864a916edee6f7f8fff8af7997e7e648f0a132b2
Craig Miller <craigmiller160@gmail.com>
2023-02-19 12:19:41 -0500

    Working on watchlist tests

--- cypress/component/Watchlists.cy.ts
+++ cypress/component/Watchlists.cy.ts
@@ -1,13 +1,40 @@
 import { tradierApi } from './api/tradier';
 import { watchlistApi } from './api/watchlists';
+import { watchlistPage } from './pages/watchlists';
 
-export {};
+const WATCHLIST_NAMES = ['ABC', 'Cryptocurrency', 'My Investments'];
 
 describe('Watchlists', () => {
 	it('renders all the watchlists on desktop', () => {
 		tradierApi.getCalendar();
 		watchlistApi.getAllWatchlists();
 		cy.mount();
+
+		watchlistPage
+			.getPageTitle()
+			.should('have.text', 'Investment Watchlists');
+		watchlistPage
+			.getWatchlistPanels()
+			.should('have.length', 3)
+			.each(($elem, index) => {
+				expect($elem.text()).eq(WATCHLIST_NAMES[index]);
+			});
+
+		watchlistPage
+			.getWatchlistPanelRenameButton(0)
+			.should('have.text', 'Rename');
+		watchlistPage.getWatchlistPanelRenameButton(1).should('not.exist');
+		watchlistPage
+			.getWatchlistPanelRenameButton(2)
+			.should('have.text', 'Rename');
+
+		watchlistPage
+			.getWatchlistPanelRemoveButton(0)
+			.should('have.text', 'Remove');
+		watchlistPage.getWatchlistPanelRemoveButton(1).should('not.exist');
+		watchlistPage
+			.getWatchlistPanelRemoveButton(2)
+			.should('have.text', 'Remove');
 		// Remove investment from watchlist, except crypto
 	});
 
