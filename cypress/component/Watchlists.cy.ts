3fe77ec005834293682adfb448c7ec8608477fc7
Craig Miller <craigmiller160@gmail.com>
2023-02-19 15:00:53 -0500

    Implemented another test

--- cypress/component/Watchlists.cy.ts
+++ cypress/component/Watchlists.cy.ts
@@ -4,7 +4,7 @@ import { watchlistPage } from './pages/watchlists';
 import { accordion } from './pages/accordion';
 import { investments } from './pages/investments';
 import { navbarPage } from './pages/navbar';
-import {coinGeckoApi} from './api/coingecko';
+import { coinGeckoApi } from './api/coingecko';
 
 const WATCHLIST_NAMES = ['ABC', 'Cryptocurrency', 'My Investments'];
 
@@ -73,7 +73,60 @@ describe('Watchlists', () => {
 		cy.mount({
 			viewport: 'mobile'
 		});
-		// Remove investment from watchlist, except crypto
-		throw new Error();
+
+		watchlistApi.getAllWatchlists();
+		tradierApi.getStockData('GHI', '1week');
+		tradierApi.getStockData('DEF', '1week');
+		tradierApi.getStockData('VTI', '1week');
+		tradierApi.getStockData('VXUS', '1week');
+		coinGeckoApi.getCryptoData('BTC', '1week');
+		coinGeckoApi.getCryptoData('ETH', '1week');
+		cy.mount();
+
+		watchlistPage
+			.getPageTitle()
+			.should('have.text', 'Investment Watchlists');
+		accordion.getPanels().should('have.length', 3);
+
+		navbarPage.desktop.getOneWeekItem().click();
+
+		cy.repeat(3, (index) => {
+			accordion
+				.getPanels()
+				.eq(index)
+				.then(($elem) => {
+					accordion
+						.getPanelTitle($elem)
+						.should('have.text', WATCHLIST_NAMES[index]);
+					if (index != 1) {
+						accordion
+							.getPanelRenameButton($elem)
+							.should('have.text', 'Rename');
+						accordion
+							.getPanelRemoveButton($elem)
+							.should('have.text', 'Remove');
+					} else {
+						accordion
+							.getPanelRenameButton($elem)
+							.should('not.exist');
+						accordion
+							.getPanelRemoveButton($elem)
+							.should('not.exist');
+					}
+				});
+		});
+
+		accordion.getPanels().eq(0).click();
+		accordion.getPanelBody(0).should('be.visible');
+		investments.getCards().should('have.length', 2);
+		investments
+			.getCardActions(0)
+			.should('have.length', 1)
+			.should('have.text', 'Remove');
+
+		accordion.getPanels().eq(1).click();
+		accordion.getPanelBody(1).should('be.visible');
+		investments.getCards().should('have.length', 2);
+		investments.getCardActions(0).should('not.exist');
 	});
 });
