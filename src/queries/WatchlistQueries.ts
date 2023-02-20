0b0da6341712c2622a790ff32831bb793848baf4
Craig Miller <craigmiller160@gmail.com>
2023-02-20 17:06:44 -0500

    Setup mutation

--- src/queries/WatchlistQueries.ts
+++ src/queries/WatchlistQueries.ts
@@ -1,18 +1,38 @@
-import { useQuery } from '@tanstack/react-query';
-import { getAllWatchlists } from '../services/WatchlistService';
+import {
+	QueryClient,
+	useMutation,
+	useQuery,
+	useQueryClient
+} from '@tanstack/react-query';
+import {
+	getAllWatchlists,
+	renameWatchlist
+} from '../services/WatchlistService';
 
 export const GET_ALL_WATCHLISTS_KEY = 'WatchlistQueries_GetAllWatchlists';
-export const RENAME_WATCHLIST_KEY = 'WatchlistQueries_RenameWatchlist';
-export const ADD_STOCK_TO_WATCHLIST_KEY =
-	'WatchlistQueries_AddStockToWatchlist';
-export const CREATE_WATCHLIST_KEY = 'WatchlistQueries_CreateWatchlist';
 export const GET_WATCHLIST_NAMES_KEY = 'WatchlistQueries_GetWatchlistNames';
-export const REMOVE_STOCK_FROM_WATCHLIST_KEY =
-	'WatchlistQueries_RemoveStockFromWatchlist';
-export const REMOVE_WATCHLIST_KEY = 'WatchlistQueries_RemoveWatchlist';
+
+const invalidateQueries = (queryClient: QueryClient) =>
+	Promise.all([
+		queryClient.invalidateQueries([GET_WATCHLIST_NAMES_KEY]),
+		queryClient.invalidateQueries([GET_ALL_WATCHLISTS_KEY])
+	]);
 
 export const useGetAllWatchlists = () =>
 	useQuery({
 		queryKey: [GET_ALL_WATCHLISTS_KEY],
 		queryFn: getAllWatchlists
 	});
+
+type RenameWatchlistParams = {
+	readonly oldName: string;
+	readonly newName: string;
+};
+
+export const useRenameWatchlist = () => {
+	const queryClient = useQueryClient();
+	useMutation<unknown, Error, RenameWatchlistParams>({
+		mutationFn: ({ oldName, newName }) => renameWatchlist(oldName, newName),
+		onSuccess: () => invalidateQueries(queryClient)
+	});
+};
