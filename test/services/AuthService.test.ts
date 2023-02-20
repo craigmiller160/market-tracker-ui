a8ddfb963655bae30b3f4a718db48afca1782a45
Craig Miller <craigmiller160@gmail.com>
2023-02-20 16:47:46 -0500

    Updated test

--- test/services/AuthService.test.ts
+++ test/services/AuthService.test.ts
@@ -2,7 +2,6 @@ import { ajaxApiFpTs } from '../../src/services/AjaxApi';
 import MockAdapter from 'axios-mock-adapter';
 import { getAuthUser } from '../../src/services/AuthService';
 import { AuthUser } from '../../src/types/auth';
-import '@relmify/jest-fp-ts';
 import { mockLocation, restoreLocation } from '../testutils/mockLocation';
 import { nanoid } from '@reduxjs/toolkit';
 
@@ -33,7 +32,7 @@ describe('AuthService', () => {
 	it('getAuthUser', async () => {
 		mockApi.onGet('/oauth/user').reply(200, authUser);
 
-		const result = await getAuthUser()();
-		expect(result).toEqualRight(authUser);
+		const result = await getAuthUser();
+		expect(result).toEqual(authUser);
 	});
 });
