35d6f7728e0ab45d399b10f40c2df518dadea801
Craig Miller <craigmiller160@gmail.com>
2023-02-20 16:43:34 -0500

    Refactored service

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,13 +1,10 @@
-import * as TE from 'fp-ts/es6/TaskEither';
 import { AuthUser } from '../types/auth';
-import { ajaxApiFpTs } from './AjaxApi';
-import { pipe } from 'fp-ts/es6/function';
+import { ajaxApi, getResponseData } from './AjaxApi';
 import { isAxiosError } from '@craigmiller160/ajax-api-fp-ts';
-import { AxiosResponse } from 'axios';
 
-export const getAuthUser = (): TE.TaskEither<Error, AuthUser> =>
-	pipe(
-		ajaxApiFpTs.get<AuthUser>({
+export const getAuthUser = (): Promise<AuthUser> =>
+	ajaxApi
+		.get<AuthUser>({
 			uri: '/oauth/user',
 			errorCustomizer: 'Error getting authenticated user',
 			suppressError: (ex: Error) => {
@@ -16,6 +13,5 @@ export const getAuthUser = (): TE.TaskEither<Error, AuthUser> =>
 				}
 				return false;
 			}
-		}),
-		TE.map((res: AxiosResponse<AuthUser>) => res.data)
-	);
+		})
+		.then(getResponseData);
