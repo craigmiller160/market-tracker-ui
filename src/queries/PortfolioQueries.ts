467f8b2036c3bc7bc19db3f6ce54cbcd01529a9a
Craig Miller <craigmiller160@gmail.com>
2023-04-29 15:13:33 -0400

    More work on react query setup

--- src/queries/PortfolioQueries.ts
+++ src/queries/PortfolioQueries.ts
@@ -1,7 +1,58 @@
-import { useQuery } from '@tanstack/react-query';
-import { getPortfolioList } from '../services/PortfolioService';
+import {
+	QueryClient,
+	useMutation,
+	UseMutationResult,
+	useQuery,
+	useQueryClient
+} from '@tanstack/react-query';
+import {
+	downloadUpdatedPortfolioData,
+	getPortfolioList
+} from '../services/PortfolioService';
 
-export const GET_PORTFOLIO_LIST_KEY = 'PortfolioQueries_GetPortfoioList';
+export const GET_PORTFOLIO_LIST_KEY = 'PortfolioQueries_GetPortfolioList';
+export const GET_STOCKS_FOR_COMBINED_PORTFOLIOS_KEY =
+	'PortfolioQueries_GetStocksForCombinedPortfolios';
+export const GET_CURRENT_SHARES_FOR_STOCK_IN_COMBINED_PORTFOLIOS_KEY =
+	'PortfolioQueries_GetCurrentSharesForStockInCombinedPortfolios';
+export const GET_SHARES_HISTORY_FOR_STOCK_IN_COMBINED_PORTFOLIOS_KEY =
+	'PortfolioQueries_GetSharesHistoryForStockInCombinedPortfolios';
+export const GET_STOCKS_FOR_PORTFOLIO_KEY =
+	'PortfolioQueries_GetStocksForPortfolio';
+export const GET_CURRENT_SHARES_FOR_STOCK_IN_PORTFOLIO_KEY =
+	'PortfolioQueries_GetCurrentSharesForStockInPortfolio';
+export const GET_SHARES_HISTORY_FOR_STOCK_IN_PORTFOLIO_KEY =
+	'PortfolioQueries_GetSharesHistoryForStockInPortfolio';
+
+const invalidateQueries = (queryClient: QueryClient) =>
+	Promise.all([
+		queryClient.invalidateQueries([GET_PORTFOLIO_LIST_KEY]),
+		queryClient.invalidateQueries([GET_STOCKS_FOR_COMBINED_PORTFOLIOS_KEY]),
+		queryClient.invalidateQueries([
+			GET_CURRENT_SHARES_FOR_STOCK_IN_COMBINED_PORTFOLIOS_KEY
+		]),
+		queryClient.invalidateQueries([
+			GET_SHARES_HISTORY_FOR_STOCK_IN_COMBINED_PORTFOLIOS_KEY
+		]),
+		queryClient.invalidateQueries([GET_STOCKS_FOR_PORTFOLIO_KEY]),
+		queryClient.invalidateQueries([
+			GET_CURRENT_SHARES_FOR_STOCK_IN_PORTFOLIO_KEY
+		]),
+		queryClient.invalidateQueries([
+			GET_SHARES_HISTORY_FOR_STOCK_IN_PORTFOLIO_KEY
+		])
+	]);
+
+export const useDownloadUpdatedPortfolioData = (): UseMutationResult<
+	unknown,
+	Error
+> => {
+	const queryClient = useQueryClient();
+	return useMutation<unknown, Error, unknown>({
+		mutationFn: downloadUpdatedPortfolioData,
+		onSuccess: () => invalidateQueries(queryClient)
+	});
+};
 
 export const useGetPortfolioList = () =>
 	useQuery({
