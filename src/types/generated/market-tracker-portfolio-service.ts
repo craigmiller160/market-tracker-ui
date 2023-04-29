0605318ec061f045ca55d13c298bc3461569c263
Craig Miller <craigmiller160@gmail.com>
2023-04-29 11:37:50 -0400

    Added types from service

---
+++ src/types/generated/market-tracker-portfolio-service.ts
@@ -0,0 +1,9 @@
+export type PortfolioResponse = {
+	readonly id: string;
+	readonly name: string;
+};
+
+export type SharesOwnedResponse = {
+	readonly date: string;
+	readonly totalShares: number;
+};
