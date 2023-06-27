16ae6827628f2a3052adb70503790bc2cd2a02c4
Craig Miller <craigmiller160@gmail.com>
2023-06-27 15:57:56 -0400

    More test improvements

--- cypress/component/Portfolios.cy.ts
+++ cypress/component/Portfolios.cy.ts
@@ -1,10 +1,12 @@
 import { tradierApi } from './api/tradier';
 import { watchlistApi } from './api/watchlists';
+import { portfolioApi } from './api/portfolios';
 
 describe('Portfolios', () => {
 	it('shows the list of portfolios', () => {
 		tradierApi.getCalendar();
 		watchlistApi.getAllWatchlists();
+		portfolioApi.getPortfolioList();
 		cy.mount();
 	});
 });
