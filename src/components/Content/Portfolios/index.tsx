d34dc308e7707fd5428e65ad0c7aa89a12ee9cb9
Craig Miller <craigmiller160@gmail.com>
2023-06-27 14:29:43 -0400

    More progress

--- src/components/Content/Portfolios/index.tsx
+++ src/components/Content/Portfolios/index.tsx
@@ -1,6 +1,19 @@
 import './Portfolios.scss';
 import { Typography } from 'antd';
 import { useGetPortfolioList } from '../../../queries/PortfolioQueries';
+import { PortfolioResponse } from '../../../types/generated/market-tracker-portfolio-service';
+import { Accordion, AccordionPanelConfig } from '../../UI/Accordion';
+
+const createPanels = (
+	data: ReadonlyArray<PortfolioResponse>
+): ReadonlyArray<AccordionPanelConfig> =>
+	data.map(
+		(res): AccordionPanelConfig => ({
+			key: res.id,
+			investments: [],
+			title: <Typography.Title level={4}>{res.name}</Typography.Title>
+		})
+	);
 
 export const Portfolios = () => {
 	const { data } = useGetPortfolioList();
@@ -8,9 +21,12 @@ export const Portfolios = () => {
 		return <></>;
 	}
 
+	const panels = createPanels(data ?? []);
+
 	return (
-		<div className="MyInvestments">
+		<div className="Portfolios">
 			<Typography.Title level={2}>Portfolios</Typography.Title>
+			<Accordion panels={panels} />
 		</div>
 	);
 };
