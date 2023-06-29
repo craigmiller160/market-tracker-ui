de93c311983b22873d88f57170ff6b8f18bf9885
Craig Miller <craigmiller160@gmail.com>
2023-06-29 15:55:50 -0400

    Implemented a lot more test logic

--- test/services/PortfolioService.test.ts
+++ test/services/PortfolioService.test.ts
@@ -1,6 +1,12 @@
 import { getDateRangeForMarketTime } from '../../src/services/PortfolioService';
 import { MarketTime } from '../../src/types/MarketTime';
 import {
+	formatHistoryDate,
+	getFiveYearHistoryStartDate,
+	getOneMonthHistoryStartDate,
+	getOneWeekHistoryStartDate,
+	getOneYearHistoryStartDate,
+	getThreeMonthHistoryStartDate,
 	getTodayEndString,
 	getTodayStartString
 } from '../../src/utils/timeUtils';
@@ -9,28 +15,44 @@ describe('PortfolioService', () => {
 	describe('getDateRangeForMarketTime', () => {
 		it('today', () => {
 			const [start, end] = getDateRangeForMarketTime(MarketTime.ONE_DAY);
-			expect(start).toEqual(getTodayStartString());
+			expect(start).toEqual(getTodayStartString()); // TODO wrong
 			expect(end).toEqual(getTodayEndString());
 		});
 
 		it('one week', () => {
-			throw new Error();
+			const [start, end] = getDateRangeForMarketTime(MarketTime.ONE_WEEK);
+			expect(start).toEqual(getOneWeekHistoryStartDate());
+			expect(end).toEqual(formatHistoryDate(new Date()));
 		});
 
 		it('one month', () => {
-			throw new Error();
+			const [start, end] = getDateRangeForMarketTime(
+				MarketTime.ONE_MONTH
+			);
+			expect(start).toEqual(getOneMonthHistoryStartDate());
+			expect(end).toEqual(formatHistoryDate(new Date()));
 		});
 
 		it('three months', () => {
-			throw new Error();
+			const [start, end] = getDateRangeForMarketTime(
+				MarketTime.THREE_MONTHS
+			);
+			expect(start).toEqual(getThreeMonthHistoryStartDate());
+			expect(end).toEqual(formatHistoryDate(new Date()));
 		});
 
 		it('one year', () => {
-			throw new Error();
+			const [start, end] = getDateRangeForMarketTime(MarketTime.ONE_YEAR);
+			expect(start).toEqual(getOneYearHistoryStartDate());
+			expect(end).toEqual(formatHistoryDate(new Date()));
 		});
 
 		it('five years', () => {
-			throw new Error();
+			const [start, end] = getDateRangeForMarketTime(
+				MarketTime.FIVE_YEARS
+			);
+			expect(start).toEqual(getFiveYearHistoryStartDate());
+			expect(end).toEqual(formatHistoryDate(new Date()));
 		});
 	});
 });
