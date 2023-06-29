222d426d05f1d1a97d1f674f35115963a8f51669
Craig Miller <craigmiller160@gmail.com>
2023-06-29 17:02:56 -0400

    Working on new portfolio data hooks

--- src/components/Content/Portfolios/useGetPortfolioInvestmentData.ts
+++ src/components/Content/Portfolios/useGetPortfolioInvestmentData.ts
@@ -1,12 +1,36 @@
-import { InvestmentInfo } from '../../../types/data/InvestmentInfo';
+import {
+	InvestmentInfo,
+	PortfolioInvestmentInfo
+} from '../../../types/data/InvestmentInfo';
 import {
 	useGetInvestmentData,
 	UseGetInvestmentDataResult
 } from '../../../queries/InvestmentQueries';
+import {
+	useGetCurrentSharesForStockInPortfolio,
+	useGetSharesHistoryForStockInPortfolio
+} from '../../../queries/PortfolioQueries';
+
+const isPortfolioInvestmentInfo = (
+	info: InvestmentInfo
+): info is PortfolioInvestmentInfo => Object.hasOwn(info, 'portfolioId');
+
+const useGetPortfolioData = (info: InvestmentInfo) => {
+	if (!isPortfolioInvestmentInfo(info)) {
+		throw new Error('InvestmentInfo is not PortfolioInvestmentInfo');
+	}
+
+	useGetCurrentSharesForStockInPortfolio(info.portfolioId, info.symbol);
+	useGetSharesHistoryForStockInPortfolio({
+		symbol: info.symbol,
+		portfolioId: info.portfolioId
+	});
+};
 
 export const useGetPortfolioInvestmentData = (
 	info: InvestmentInfo
 ): UseGetInvestmentDataResult => {
+	useGetPortfolioData(info);
 	const getInvestmentDataResult = useGetInvestmentData(info);
 	// TODO need to figure out how to integrate portfolio stuff
 	return getInvestmentDataResult;
