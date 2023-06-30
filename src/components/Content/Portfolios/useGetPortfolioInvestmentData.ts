b11edc64c202c7752ecbea4bf5b64239ee572e46
Craig Miller <craigmiller160@gmail.com>
2023-06-30 10:14:16 -0400

    Implemented portfolio data hook

--- src/components/Content/Portfolios/useGetPortfolioInvestmentData.ts
+++ src/components/Content/Portfolios/useGetPortfolioInvestmentData.ts
@@ -12,20 +12,40 @@ import {
 } from '../../../queries/PortfolioQueries';
 import { useSelector } from 'react-redux';
 import { timeValueSelector } from '../../../store/marketSettings/selectors';
+import { SharesOwnedResponse } from '../../../types/generated/market-tracker-portfolio-service';
 
 const isPortfolioInvestmentInfo = (
 	info: InvestmentInfo
 ): info is PortfolioInvestmentInfo => Object.hasOwn(info, 'portfolioId');
 
-const useGetPortfolioData = (info: InvestmentInfo) => {
+type UseGetPortfolioDataReturn = Readonly<{
+	currentData?: SharesOwnedResponse;
+	historyData?: ReadonlyArray<SharesOwnedResponse>;
+	isFetching: boolean;
+}>;
+
+const useGetPortfolioData = (
+	info: InvestmentInfo
+): UseGetPortfolioDataReturn => {
 	if (!isPortfolioInvestmentInfo(info)) {
 		throw new Error('InvestmentInfo is not PortfolioInvestmentInfo');
 	}
 
 	const time = useSelector(timeValueSelector);
 
-	useGetCurrentSharesForStockInPortfolio(info.portfolioId, info.symbol);
-	useGetSharesHistoryForStockInPortfolio(info.portfolioId, info.symbol, time);
+	const { data: currentData, isFetching: currentIsFetching } =
+		useGetCurrentSharesForStockInPortfolio(info.portfolioId, info.symbol);
+	const { data: historyData, isFetching: historyIsFetching } =
+		useGetSharesHistoryForStockInPortfolio(
+			info.portfolioId,
+			info.symbol,
+			time
+		);
+	return {
+		currentData,
+		historyData,
+		isFetching: currentIsFetching || historyIsFetching
+	};
 };
 
 export const useGetPortfolioInvestmentData = (
