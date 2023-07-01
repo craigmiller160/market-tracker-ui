ffa489555f79eb056ff01c42ef17aa30f8a7e8d5
Craig Miller <craigmiller160@gmail.com>
2023-07-01 19:43:44 -0400

    Refactored service worker to be typescript and how it is registered

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import { App } from './components/App';
-import { store } from './store';
-import { notificationSlice } from './store/notification/slice';
+// import { store } from './store';
+// import { notificationSlice } from './store/notification/slice';
 import { BrowserRouter } from 'react-router-dom';
 import { MarketTrackerKeycloakProvider } from './components/keycloak/MarketTrackerKeycloakProvider';
 
@@ -19,17 +19,17 @@ root.render(
 	</BrowserRouter>
 );
 
-if ('serviceWorker' in navigator) {
-	window.addEventListener('load', () => {
-		navigator.serviceWorker
-			.register('/market-tracker/service-worker.js')
-			.then((registration) => registration.update())
-			.catch((ex) =>
-				store.dispatch(
-					notificationSlice.actions.addError(
-						`Service Worker registration failed: ${ex.message}`
-					)
-				)
-			);
-	});
-}
+// if ('serviceWorker' in navigator) {
+// 	window.addEventListener('load', () => {
+// 		navigator.serviceWorker
+// 			.register('/market-tracker/service-worker.js')
+// 			.then((registration) => registration.update())
+// 			.catch((ex) =>
+// 				store.dispatch(
+// 					notificationSlice.actions.addError(
+// 						`Service Worker registration failed: ${ex.message}`
+// 					)
+// 				)
+// 			);
+// 	});
+// }
