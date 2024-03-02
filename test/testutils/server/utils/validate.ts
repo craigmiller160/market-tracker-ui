c19d583cd9cc514c5a6a48b4859c73a3e3c5cca7
Craig Miller <craigmiller160@gmail.com>
2024-03-02 12:01:18 -0500

    Deleted old server

--- test/testutils/server/utils/validate.ts
+++
@@ -1,6 +0,0 @@
-import { Response } from 'miragejs';
-
-export const validationError = (message: string): Response => {
-	console.error(message);
-	return new Response(400, {}, 'Request Validation Failed');
-};
