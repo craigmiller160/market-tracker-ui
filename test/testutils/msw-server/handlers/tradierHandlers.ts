747d7bb641665fff5a11bc8a237194a6443405e2
Craig Miller <craigmiller160@gmail.com>
2024-03-02 12:00:57 -0500

    More handler work

--- test/testutils/msw-server/handlers/tradierHandlers.ts
+++ test/testutils/msw-server/handlers/tradierHandlers.ts
@@ -4,10 +4,14 @@ import {
 	http,
 	HttpHandler,
 	HttpResponse,
-	type PathParams
+	type PathParams,
+	type StrictResponse
 } from 'msw';
 import * as Time from '@craigmiller160/ts-functions/Time';
 import { validationError } from '../utils/validate';
+import type { TradierSeries } from '../../../../src/types/tradier/timesales';
+import type { TradierQuotes } from '../../../../src/types/tradier/quotes';
+import type { TradierHistory } from '../../../../src/types/tradier/history';
 
 const getTodayYear = () => Time.format('yyyy')(new Date());
 const getTodayMonth = () => Time.format('MM')(new Date());
@@ -38,7 +42,7 @@ export const createTradierHandlers = (
 
 	const getTimesalesHandler = http.get(
 		'http://localhost/market-tracker/api/tradier/markets/timesales',
-		({ request }) => {
+		({ request }): StrictResponse<TradierSeries | string> => {
 			const queryParams = new URL(request.url).searchParams;
 			const start = queryParams.get('start');
 			const end = queryParams.get('end');
@@ -59,5 +63,56 @@ export const createTradierHandlers = (
 			return HttpResponse.json(timesale);
 		}
 	);
-	return [getCalendarHandler, getTimesalesHandler];
+
+	const getQuotesHandler = http.get(
+		'http://localhost/market-tracker/api/tradier/markets/quotas',
+		({ request }): StrictResponse<TradierQuotes | string> => {
+			const queryParams = new URL(request.url).searchParams;
+			const symbols = queryParams.get('symbols');
+			if (!symbols) {
+				return validationError('Missing symbols param');
+			}
+
+			const quote = database.data.tradier.quotes[symbols];
+			if (!quote) {
+				return validationError(`No quote for symbol: ${symbols}`);
+			}
+			return HttpResponse.json(quote);
+		}
+	);
+
+	const getHistoryHandler = http.get(
+		'http://localhost/market-tracker/api/tradier/markets/history',
+		({ request }): StrictResponse<TradierHistory | string> => {
+			const queryParams = new URL(request.url).searchParams;
+			const start = queryParams.get('start');
+			const end = queryParams.get('end');
+			const symbol = queryParams.get('symbol');
+			const interval = queryParams.get('interval');
+			if (!start) {
+				return validationError(`Missing start param`);
+			}
+			if (!end) {
+				return validationError('Missing end param');
+			}
+			if (!symbol) {
+				return validationError('Missing symbol param');
+			}
+			if (!interval) {
+				return validationError('Missing interval param');
+			}
+
+			const history = database.data.tradier.history[symbol];
+			if (!history) {
+				return validationError(`No history for symbol: ${symbol}`);
+			}
+			return HttpResponse.json(history);
+		}
+	);
+	return [
+		getCalendarHandler,
+		getTimesalesHandler,
+		getQuotesHandler,
+		getHistoryHandler
+	];
 };
