2d5d29b23b004f4fb2611dd7603f815e59cc2d28
Craig Miller <craigmiller160@gmail.com>
2024-03-02 08:39:15 -0500

    More handlers

--- test/testutils/msw-server/handlers/watchlistHandlers.ts
+++ test/testutils/msw-server/handlers/watchlistHandlers.ts
@@ -1,10 +1,12 @@
-import type { Database } from '../Database';
-import { type RequestHandler, http, HttpResponse } from 'msw';
+import { type Database, ensureDbRecord } from '../Database';
+import { type RequestHandler, http, HttpResponse, type PathParams } from 'msw';
+import type { Watchlist } from '../../../../src/types/Watchlist';
+import { castDraft } from 'immer';
 
 export const createWatchlistHandlers = (
 	database: Database
 ): ReadonlyArray<RequestHandler> => {
-	const watchlistNamesHandler = http.get(
+	const getWatchlistNamesHandler = http.get(
 		'http://localhost/market-tracker/api/watchlists/names',
 		() => {
 			const names = database.data.watchlists.map(
@@ -14,5 +16,28 @@ export const createWatchlistHandlers = (
 		}
 	);
 
-	return [watchlistNamesHandler];
+	const createWatchlistHandler = http.post<PathParams, Watchlist>(
+		'http://localhost/market-tracker/api/watchlists',
+		async ({ request }) => {
+			const body = await request.json();
+			const dbWatchlist = ensureDbRecord(body);
+			database.updateData((draft) => {
+				draft.watchlists.push(castDraft(dbWatchlist));
+			});
+			return HttpResponse.json(dbWatchlist);
+		}
+	);
+
+	const getAllWatchlistsHandler = http.get(
+		'http://localhost/market-tracker/api/watchlists/all',
+		() => {
+			return HttpResponse.json(database.data.watchlists);
+		}
+	);
+
+	return [
+		getWatchlistNamesHandler,
+		createWatchlistHandler,
+		getAllWatchlistsHandler
+	];
 };
