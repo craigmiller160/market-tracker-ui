73f9d808c8d50689370f43b91bd35bd64a55c954
Craig Miller <craigmiller160@gmail.com>
2024-03-02 11:54:14 -0500

    Working on handlers

--- test/testutils/msw-server/handlers/tradierHandlers.ts
+++ test/testutils/msw-server/handlers/tradierHandlers.ts
@@ -1,8 +1,39 @@
 import type { Database } from '../Database';
-import { HttpHandler } from 'msw';
+import {
+	DefaultBodyType,
+	http,
+	HttpHandler,
+	HttpResponse,
+	type PathParams
+} from 'msw';
+import * as Time from '@craigmiller160/ts-functions/Time';
+import { validationError } from '../../server/utils/validate';
+
+const getTodayYear = () => Time.format('yyyy')(new Date());
+const getTodayMonth = () => Time.format('MM')(new Date());
 
 export const createTradierHandlers = (
 	database: Database
 ): ReadonlyArray<HttpHandler> => {
-    return [];
+	const getCalendarHandler = http.get<PathParams, DefaultBodyType>(
+		'http://localhost/market-tracker/api/tradier/markets/calendar',
+		({ request }) => {
+			const queryParams = new URL(request.url).searchParams;
+			const year = queryParams.get('year');
+			const month = queryParams.get('month');
+
+			if (year !== getTodayYear()) {
+				return validationError(
+					`Incorrect year param. Expected: ${getTodayYear()} Actual: ${year}`
+				);
+			}
+			if (month !== getTodayMonth()) {
+				return validationError(
+					`Incorrect month param. Expected: ${getTodayMonth()} Actual: ${month}`
+				);
+			}
+			HttpResponse.json(database.data.tradier.calendar);
+		}
+	);
+	return [getCalendarHandler];
 };
