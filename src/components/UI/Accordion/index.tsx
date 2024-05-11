bafb6be91202745df32c15b9d4522938a733bba7
Craig Miller <craigmiller160@gmail.com>
2024-05-11 17:10:00 -0400

    Refactoring how the load investment functionality is provided to the cards to maximize flexibility

--- src/components/UI/Accordion/index.tsx
+++ src/components/UI/Accordion/index.tsx
@@ -2,28 +2,45 @@ import { Collapse } from 'antd';
 import { type ReactNode } from 'react';
 import { type AccordionInvestment } from './AccordionInvestment';
 import { AccordionPanel } from './AccordionPanel';
+import {
+	InvestmentCardDataLoadingContext,
+	type UseLoadInvestmentData
+} from '../../Content/common/InvestmentCard/InvestmentCardDataLoadingContext';
 import './Accordion.scss';
 
-export interface AccordionPanelConfig {
-	readonly title: ReactNode;
-	readonly key: string;
-	readonly actions?: ReactNode;
-	readonly investments: ReadonlyArray<AccordionInvestment>;
-}
+export type AccordionPanelConfig = Readonly<{
+	title: ReactNode;
+	key: string;
+	actions?: ReactNode;
+	investments: ReadonlyArray<AccordionInvestment>;
+	useLoadInvestmentData?: UseLoadInvestmentData;
+}>;
 
 type Props = Readonly<{
 	id?: string;
 	panels: ReadonlyArray<AccordionPanelConfig>;
 }>;
 
-const panelConfigToPanels = (config: AccordionPanelConfig) => (
-	<AccordionPanel
-		title={config.title}
-		key={config.key}
-		actions={config.actions}
-		investments={config.investments}
-	/>
-);
+const panelConfigToPanels = (config: AccordionPanelConfig) => {
+	const Panel = (
+		<AccordionPanel
+			title={config.title}
+			key={config.key}
+			actions={config.actions}
+			investments={config.investments}
+		/>
+	);
+	if (config.useLoadInvestmentData) {
+		return (
+			<InvestmentCardDataLoadingContext.Provider
+				value={config.useLoadInvestmentData}
+			>
+				{Panel}
+			</InvestmentCardDataLoadingContext.Provider>
+		);
+	}
+	return Panel;
+};
 
 export const Accordion = (props: Props) => {
 	const { panels } = props;
