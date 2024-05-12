127cb760aeb3a407d6b00bbb151b33d8c05a4fd6
Craig Miller <craigmiller160@gmail.com>
2024-05-11 21:19:31 -0400

    Updated

--- src/types/data/InvestmentInfo.ts
+++ src/types/data/InvestmentInfo.ts
@@ -1,10 +1,10 @@
 import { InvestmentType } from './InvestmentType';
 
-export interface InvestmentInfo {
-	readonly symbol: string;
-	readonly name: string;
-	readonly type: InvestmentType;
-}
+export type InvestmentInfo = Readonly<{
+	symbol: string;
+	name: string;
+	type: InvestmentType;
+}>;
 
 export type PortfolioInvestmentInfo = InvestmentInfo & {
 	readonly portfolioId: string;
