b7dd3c2dfebf008165097750946e22962fe97a6e
Craig Miller <craigmiller160@gmail.com>
2024-05-18 15:14:15 -0400

    Refactoring common logic

--- src/components/Content/Portfolios/hooks/useGetPortfolioInvestmentData.ts
+++ src/components/Content/Portfolios/hooks/useGetPortfolioInvestmentData.ts
@@ -11,11 +11,8 @@ import { useSelector } from 'react-redux';
 import { timeValueSelector } from '../../../../store/marketSettings/selectors';
 import { type SharesOwnedResponse } from '../../../../types/generated/market-tracker-portfolio-service';
 import { useMemo } from 'react';
-import { type InvestmentData } from '../../../../types/data/InvestmentData';
 import { MarketTime } from '../../../../types/MarketTime';
-import { type HistoryRecord } from '../../../../types/history';
-import { match, P } from 'ts-pattern';
-import { isPortfolioInvestmentInfo } from './common';
+import { isPortfolioInvestmentInfo, mergeInvestmentData } from './common';
 
 type UseGetPortfolioDataReturn = Readonly<{
 	currentData?: SharesOwnedResponse;
@@ -51,75 +48,6 @@ const useGetPortfolioData = (
 		historyData,
 		error: currentError ?? historyError ?? undefined,
 		isFetching: currentIsFetching || historyIsFetching
-	};
-};
-
-type PortfolioHistoryFinder = (
-	rec: HistoryRecord
-) => (pRec: SharesOwnedResponse) => boolean;
-const getPortfolioHistoryFinder = (time: MarketTime): PortfolioHistoryFinder =>
-	match<MarketTime, PortfolioHistoryFinder>(time)
-		.with(MarketTime.ONE_DAY, () => () => () => true)
-		.with(
-			P.union(
-				MarketTime.ONE_WEEK,
-				MarketTime.ONE_MONTH,
-				MarketTime.THREE_MONTHS,
-				MarketTime.ONE_YEAR,
-				MarketTime.FIVE_YEARS
-			),
-			() => (rec) => (pRec) => rec.date === pRec.date
-		)
-		.run();
-
-const mergeHistory = (
-	time: MarketTime,
-	investmentHistory: ReadonlyArray<HistoryRecord>,
-	portfolioHistory: ReadonlyArray<SharesOwnedResponse>
-): ReadonlyArray<HistoryRecord> => {
-	const portfolioHistoryFinder = getPortfolioHistoryFinder(time);
-
-	return investmentHistory.map((record): HistoryRecord => {
-		const portfolioRecord = portfolioHistory.find(
-			portfolioHistoryFinder(record)
-		);
-
-		return {
-			...record,
-			price: record.price * (portfolioRecord?.totalShares ?? 0)
-		};
-	});
-};
-
-const mergeInvestmentData = (
-	time: MarketTime,
-	investmentData?: InvestmentData,
-	portfolioCurrentData?: SharesOwnedResponse,
-	portfolioHistoryData?: ReadonlyArray<SharesOwnedResponse>
-): InvestmentData | undefined => {
-	if (
-		investmentData === undefined ||
-		portfolioCurrentData === undefined ||
-		portfolioHistoryData === undefined
-	) {
-		return undefined;
-	}
-
-	const startPrice =
-		portfolioHistoryData[0].totalShares * investmentData.startPrice;
-	const currentPrice =
-		portfolioCurrentData.totalShares * investmentData.currentPrice;
-	const history = mergeHistory(
-		time,
-		investmentData.history,
-		portfolioHistoryData
-	);
-
-	return {
-		name: investmentData.name,
-		startPrice,
-		currentPrice,
-		history
 	};
 };
 
