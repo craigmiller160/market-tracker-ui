6db02617e620060484a239b18bfbccd6360f9209
Craig Miller <craigmiller160@gmail.com>
2024-05-18 21:06:10 -0400

    More work on test data

--- test/testutils/support/aggregate-queries/portfolio-data.ts
+++ test/testutils/support/aggregate-queries/portfolio-data.ts
@@ -9,3 +9,81 @@ export const vxusCurrent: SharesOwnedResponse = {
 	date: '2024-05-18',
 	totalShares: 272.599
 };
+
+export const vtiTodayHistory: ReadonlyArray<SharesOwnedResponse> = [
+	{ date: '2024-05-18', totalShares: 290.409 }
+];
+
+export const vxusTodayHistory: ReadonlyArray<SharesOwnedResponse> = [
+	{ date: '2024-05-18', totalShares: 272.599 }
+];
+
+export const vtiOneWeekHistory: ReadonlyArray<SharesOwnedResponse> = [
+	{
+		date: '2024-05-11',
+		totalShares: 290.409
+	},
+	{
+		date: '2024-05-12',
+		totalShares: 290.409
+	},
+	{
+		date: '2024-05-13',
+		totalShares: 290.409
+	},
+	{
+		date: '2024-05-14',
+		totalShares: 290.409
+	},
+	{
+		date: '2024-05-15',
+		totalShares: 290.409
+	},
+	{
+		date: '2024-05-16',
+		totalShares: 290.409
+	},
+	{
+		date: '2024-05-17',
+		totalShares: 290.409
+	},
+	{
+		date: '2024-05-18',
+		totalShares: 290.409
+	}
+];
+
+export const vxusOneWeekHistory: ReadonlyArray<SharesOwnedResponse> = [
+	{
+		date: '2024-05-11',
+		totalShares: 272.599
+	},
+	{
+		date: '2024-05-12',
+		totalShares: 272.599
+	},
+	{
+		date: '2024-05-13',
+		totalShares: 272.599
+	},
+	{
+		date: '2024-05-14',
+		totalShares: 272.599
+	},
+	{
+		date: '2024-05-15',
+		totalShares: 272.599
+	},
+	{
+		date: '2024-05-16',
+		totalShares: 272.599
+	},
+	{
+		date: '2024-05-17',
+		totalShares: 272.599
+	},
+	{
+		date: '2024-05-18',
+		totalShares: 272.599
+	}
+];
