c25d5a02d7313b8cd826f119977c7800860c4482
Craig Miller <craigmiller160@gmail.com>
2024-05-18 22:05:37 -0400

    More progress with tests

--- test/queries/PortfolioAggregateQueries.test.tsx
+++ test/queries/PortfolioAggregateQueries.test.tsx
@@ -1,33 +1,58 @@
-import { test } from 'vitest';
+import { expect, test } from 'vitest';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { MarketTime, marketTimeToMenuKey } from '../../src/types/MarketTime';
 import { prepareAggregateQueryMswHandlers } from '../testutils/support/aggregate-queries/portfolio-msw-handlers';
 import { createStore, type StoreType } from '../../src/store/createStore';
 import { Provider } from 'react-redux';
-import { render } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import type { PropsWithChildren } from 'react';
+import { useGetAggregateCurrentSharesForStocksInPortfolio } from '../../src/queries/PortfolioAggregateQueries';
 
 const queryClient = new QueryClient();
 
-const QueryValidationComponent = () => <div />;
+const CurrentSharesComponent = () => {
+	const { data, isLoading, error } =
+		useGetAggregateCurrentSharesForStocksInPortfolio('id', ['VTI', 'VXUS']);
+
+	return (
+		<div>
+			<p>Is Loading: {`${isLoading}`}</p>
+			<p>Has Error: {`${error !== null}`}</p>
+		</div>
+	);
+};
 
 type RootComponentProps = Readonly<{
 	store: StoreType;
 }>;
-const RootComponent = ({ store }: RootComponentProps) => (
+const RootComponent = ({
+	store,
+	children
+}: PropsWithChildren<RootComponentProps>) => (
 	<div>
 		<Provider store={store}>
 			<QueryClientProvider client={queryClient}>
-				<QueryValidationComponent />
+				{children}
 			</QueryClientProvider>
 		</Provider>
 	</div>
 );
 
-test('validates useGetAggregateCurrentSharesForStocksInPortfolio', () => {
+test('validates useGetAggregateCurrentSharesForStocksInPortfolio', async () => {
 	prepareAggregateQueryMswHandlers();
 	const store = createStore();
-	render(<RootComponent store={store} />);
-	throw new Error();
+
+	render(
+		<RootComponent store={store}>
+			<CurrentSharesComponent />
+		</RootComponent>
+	);
+	await waitFor(() =>
+		expect(screen.getByText(/Is Loading/)).toHaveTextContent(
+			'Is Loading: false'
+		)
+	);
+	expect(screen.getByText('Has Error: false')).toBeVisible();
 });
 
 test.each<MarketTime>([MarketTime.ONE_DAY, MarketTime.ONE_WEEK])(
@@ -42,7 +67,6 @@ test.each<MarketTime>([MarketTime.ONE_DAY, MarketTime.ONE_WEEK])(
 				}
 			}
 		});
-		render(<RootComponent store={store} />);
 		throw new Error();
 	}
 );
