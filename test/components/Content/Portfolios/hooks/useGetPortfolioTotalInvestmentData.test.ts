c6e88ab488a3c083aa46cbfdf5b34e5c97ce79ec
Craig Miller <craigmiller160@gmail.com>
2024-05-19 10:17:27 -0400

    Progress with tests

--- test/components/Content/Portfolios/hooks/useGetPortfolioTotalInvestmentData.test.ts
+++ test/components/Content/Portfolios/hooks/useGetPortfolioTotalInvestmentData.test.ts
@@ -1,4 +1,4 @@
-import { test } from 'vitest';
+import { expect, test } from 'vitest';
 import { MarketTime } from '../../../../../src/types/MarketTime';
 import type { AggregateInvestmentData } from '../../../../../src/queries/InvestmentAggregateQueries';
 import { match } from 'ts-pattern';
@@ -9,14 +9,15 @@ import {
 	expectedVxusDataNoHistory
 } from '../../../../testutils/support/aggregate-queries/tradier-data';
 import {
-  type AggregatePortfolioData,
-  mergeTotalInvestmentData
+	type AggregatePortfolioData,
+	mergeTotalInvestmentData
 } from '../../../../../src/components/Content/Portfolios/hooks/useGetPortfolioTotalInvestmentData';
 import {
 	aggregateCurrent,
 	aggregateOneWeekHistory,
 	aggregateTodayHistory
 } from '../../../../testutils/support/aggregate-queries/portfolio-data';
+import type { InvestmentData } from '../../../../../src/types/data/InvestmentData';
 
 const getInvestmentData = (time: MarketTime): AggregateInvestmentData =>
 	match<MarketTime, AggregateInvestmentData>(time)
@@ -42,12 +43,71 @@ const getPortfolioData = (time: MarketTime): AggregatePortfolioData =>
 		}))
 		.run();
 
+const getExpectedStartPrice = (
+	time: MarketTime,
+	investmentData: AggregateInvestmentData,
+	portfolioData: AggregatePortfolioData
+): number =>
+	match<MarketTime, number>(time)
+		.with(MarketTime.ONE_DAY, () => {
+			const totalVti =
+				investmentData.VTI.startPrice *
+				portfolioData.current.VTI.totalShares;
+			const totalVxus =
+				investmentData.VXUS.startPrice *
+				portfolioData.current.VXUS.totalShares;
+			return totalVti + totalVxus;
+		})
+		.with(MarketTime.ONE_WEEK, () => {
+			const totalVti =
+				investmentData.VTI.history[0].price *
+				portfolioData.current.VTI.totalShares;
+			const totalVxus =
+				investmentData.VXUS.history[0].price *
+				portfolioData.current.VXUS.totalShares;
+			return totalVti + totalVxus;
+		})
+		.run();
+
+const getExpectedCurrentPrice = (
+	investmentData: AggregateInvestmentData,
+	portfolioData: AggregatePortfolioData
+): number => {
+	const vtiTotal =
+		investmentData.VTI.currentPrice * portfolioData.current.VTI.totalShares;
+	const vxusTotal =
+		investmentData.VXUS.currentPrice *
+		portfolioData.current.VXUS.totalShares;
+	return vtiTotal + vxusTotal;
+};
+
 test.each<MarketTime>([MarketTime.ONE_DAY, MarketTime.ONE_WEEK])(
 	'validate mergeTotalInvestmentData',
 	(time) => {
 		const investmentData = getInvestmentData(time);
 		const portfolioData = getPortfolioData(time);
-		const result = mergeTotalInvestmentData(time, investmentData, portfolioData);
-    console.log('RESULT', result); // TODO delete this
+		const result = mergeTotalInvestmentData(
+			time,
+			investmentData,
+			portfolioData
+		);
+
+		const expectedStartPrice = getExpectedStartPrice(
+			time,
+			investmentData,
+			portfolioData
+		);
+		const expectedCurrentPrice = getExpectedCurrentPrice(
+			investmentData,
+			portfolioData
+		);
+
+		console.log('RESULT', result); // TODO delete this
+		expect(result).toEqual<InvestmentData>({
+			name: 'Portfolio',
+			startPrice: expectedStartPrice,
+			currentPrice: expectedCurrentPrice,
+			history: expect.anything()
+		});
 	}
 );
