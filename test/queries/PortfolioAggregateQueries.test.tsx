c81d1f9856412c2e6286f1b52b4223d2b8a7c398
Craig Miller <craigmiller160@gmail.com>
2024-05-18 20:28:26 -0400

    Stubbed out more tests

---
+++ test/queries/PortfolioAggregateQueries.test.tsx
@@ -0,0 +1,5 @@
+import { test } from 'vitest';
+
+test.fails('validates useGetAggregateCurrentSharesForStocksInPortfolio');
+
+test.fails('validates useGetAggregateSharesHistoryForStocksInPortfolio');
